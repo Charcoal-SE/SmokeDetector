ea11263a3af5cb71801fa8f02cab648bd0f36da0
hichris1234 <christopherncarmel@hotmail.com>
2014-01-30 16:15:51 -0500

    Create findspam.py

---
+++ findspam.py
@@ -0,0 +1,7 @@
+class FindSpam:
+  @staticmethod
+  def testtitle(title):
+    if 'vashikaran' in title or 'baba' in title or 'theotherthingy' in title:
+      # magic if matches word
+    elif title.upper() == title:
+      # magic if all caps
