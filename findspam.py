55f9ca8745c094496fec9b75e804cc871f20dd4f
hichris1234 <christopherncarmel@hotmail.com>
2014-01-30 17:06:52 -0500

    Update findspam.py

--- findspam.py
+++ findspam.py
@@ -2,9 +2,12 @@ import re
 class FindSpam:
   @staticmethod
   def testtitle(title):
+    result = []
     p = re.compile('\d{10}')
     m = p.match(title)
     if 'vashikaran' in title or 'baba' in title or p:
+      result.append('Matched keyword')
       # magic if matches word
     elif title.upper() == title:
+      result.append('All caps title')
       # magic if all caps
