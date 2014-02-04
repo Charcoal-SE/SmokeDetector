46f55176aeea786c60b1b56a6af7c7d3e3fc9a7f
Jan Dvorak <honnza@gmail.com>
2014-02-04 10:29:45 +0100

    fixed a regex - baba(ji?) matches babaj and babaji

--- findspam.py
+++ findspam.py
@@ -4,7 +4,7 @@ import re
 class FindSpam:
   @staticmethod
   def testtitle(title):
-    regexes=["\\b(baba(ji?)|vashikaran|fashion|here is|porn)\\b","\\+\\d{10}","\\+?\\d{2}\\s?\\d{8}","\\b(asshole|crap|fag|fuck|idiot|shit|whore)s?\\b"]
+    regexes=["\\b(baba(ji)?|vashikaran|fashion|here is|porn)\\b","\\+\\d{10}","\\+?\\d{2}\\s?\\d{8}","\\b(asshole|crap|fag|fuck|idiot|shit|whore)s?\\b"]
     result = []
     p = [not not re.compile(s).search(title) for s in regexes]
     if 'vashikaran' in title or 'baba' in title or True in p:
