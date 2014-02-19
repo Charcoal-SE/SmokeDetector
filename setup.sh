bff26174494c822097b8dbb12025974268bd5bcf
Manish Goregaokar <manishsmail@gmail.com>
2014-02-19 15:40:24 +0000

    +setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/Charcoal-SE/SmokeDetector.git
+cd SmokeDetector
+git submodule init
+git submodule update
+sudo pip install beautifulsoup
+sudo pip install requests --upgrade
+sudo pip installwebsocket-client --upgrade
