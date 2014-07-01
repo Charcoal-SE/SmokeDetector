2a41077720a7991b7ee875b0fb646472155fd5ac
ProgramFOX <programfox@hotmail.be>
2014-07-01 16:33:27 +0200

    Added case insensitivity to title regexes and improved 'Offensive title' regex

--- findspam.py
+++ findspam.py
@@ -7,7 +7,7 @@ class FindSpam:
     'sites': [], 'reason': "Bad keyword detected"},
    {'regex': "\\+\\d{10}|\\+?\\d{2}[\\s\\-]?\\d{8,1o}", 'all': True, 
     'sites': ["patents.stackexchange.com"], 'reason': "Phone number detected"},
-   {'regex': "(?i)\\b([Nn]igga|[Nn]igger|niga|[Aa]sshole|crap|fag|[Ff]uck|idiot|[Ss]hit|[Ww]hore)s?\\b", 'all': True,
+   {'regex': "(?i)\\b([Nn]igga|[Nn]igger|niga|[Aa]sshole|crap|fag|[Ff]uck(ing?)?|idiot|[Ss]hit|[Ww]hore)s?\\b", 'all': True,
     'sites': [], 'reason': "Offensive title detected",'insensitive':True},
    {'regex': "^(?=.*[A-Z])[^a-z]*$", 'all': True, 'sites': [], 'reason': "All-caps title"},
    {'regex': "(?i)\\b(diet|body|weight|aging)s?\\b", 'all': True, 'sites': [], 'reason': "anti-aging spam"}
@@ -24,7 +24,7 @@ class FindSpam:
 
   @staticmethod
   def testtitle(title):
-    regexes=["\\b(baba(ji)?|vashikaran|fashion|here is|porn)\\b","\\+\\d{10}","\\+?\\d{2}\\s?\\d{8}","\\b(asshole|crap|fag|fuck|idiot|shit|whore)s?\\b"]
+    regexes=["(?i)\\b(baba(ji)?|vashikaran|fashion|here is|porn)\\b","\\+\\d{10}","\\+?\\d{2}\\s?\\d{8}","(?i)\\b(asshole|crap|fag|fuck(ing?)?|idiot|shit|whore)s?\\b"]
     result = []
     p = [not not re.compile(s).search(title) for s in regexes]
     if 'vashikaran' in title or 'baba' in title or True in p:
