772cb3d3d30c769cc4e4d6287b99aa19f96cc239
Vincentyification <Vincentyification@users.noreply.github.com>
2014-08-29 00:01:09 +0800

    Update findspam.py

--- findspam.py
+++ findspam.py
@@ -7,7 +7,7 @@ class FindSpam:
     'sites': [], 'reason': "Bad keyword detected"},
    {'regex': "\\+\\d{10}|\\+?\\d{2}[\\s\\-]?\\d{8,1o}", 'all': True,
     'sites': ["patents.stackexchange.com"], 'reason': "Phone number detected"},
-   {'regex': "(?i)\\b(nigg?a|nigger|asshole|crap|fag|fuck(ing?)?|idiot|shit|whore)s?\\b", 'all': True,
+   {'regex': "(?i)\\b(nigg?(a|er)|asshole|crap|fag|fuck(ing?)?|idiot|shit|whore)s?\\b", 'all': True,
     'sites': [], 'reason': "Offensive title detected",'insensitive':True},
    {'regex': "^(?=.*[A-Z])[^a-z]*$", 'all': True, 'sites': [], 'reason': "All-caps title"}
   ]
