d8f01e9d05c80499d3ec5438ab5db4a550273b1d
ProgramFOX <programfox@hotmail.be>
2014-09-07 17:50:57 +0200

    Changed BeautifulSoup version from 3 to 4
    Changed BeautifulSoup version from 3 to 4 because using version 3 results in an error: http://chat.meta.stackexchange.com/transcript/message/2381831#2381831

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ git clone https://github.com/Charcoal-SE/SmokeDetector.git
 cd SmokeDetector
 git submodule init
 git submodule update
-sudo pip install beautifulsoup
+sudo pip install beautifulsoup4
 sudo pip install requests --upgrade
 sudo pip install websocket-client --upgrade
