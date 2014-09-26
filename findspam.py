850d066899e90f067df57e94f94909d9d9472ec9
AWegnerGitHub <andy@andrewwegner.com>
2014-09-26 14:48:02 -0500

    Updates to mesh current regex's with new phonenumbers logic
    Removes old testtitle method that doesn't exist in current Smokey
    but did at the time of the original pull request.
    
    Leaves print statement in function (correct spelling) to be used for
    troubleshooting during initial deployment.

--- findspam.py
+++ findspam.py
@@ -1,16 +1,17 @@
 import re
 import phonenumbers
 
-
 class FindSpam:
   rules = [
-   {'regex': "(?i)\\b(baba(ji)?|nike|vashikaran|here is|porn)\\b", 'all': True,
+   {'regex': "(?i)\\b(baba(ji)?|nike|vashikaran|sumer|kolcak|porn|molvi|judi bola|ituBola.com|lost lover|11s|acai|skin care)\\b", 'all': True,
     'sites': [], 'reason': "Bad keyword detected"},
-   {'regex': "\\+\\d{10}|\\+?\\d{2}[\\s\\-]?\\d{8,11}", 'all': True, 
+   {'regex': "(?i)\\b(weight loss)\\b", 'all': True,
+    'sites': ["fitness.stackexchange.com"], 'reason': "Bad keyword detected"},
+   {'regex': "\\d(?:_*\\d){9}|\\+?\\d_*\\d[\\s\\-]?(_*\\d){8,10}", 'all': True,
     'sites': ["patents.stackexchange.com"], 'reason': "Phone number detected", 'validation_method': 'phonenumbers'},
-   {'regex': "(?i)\\b([Nn]igga|[Nn]igger|niga|[Aa]sshole|crap|fag|[Ff]uck|idiot|[Ss]hit|[Ww]hore)s?\\b", 'all': True,
+   {'regex': "(?i)\\b(nigg?(a|er)|asshole|crap|fag|fuck(ing?)?|idiot|shit|whore)s?\\b", 'all': True,
     'sites': [], 'reason': "Offensive title detected",'insensitive':True},
-   {'regex': "^[A-Z0-9\\(\\)\\.\\-\\?\\s'\"]*$", 'all': True, 'sites': [], 'reason': "All-caps title"}
+   {'regex': "^(?=.*[A-Z])[^a-z]*$", 'all': True, 'sites': [], 'reason': "All-caps title"}
   ]
 
   @staticmethod
@@ -20,20 +21,10 @@ class FindSpam:
       if rule['all'] != (site in rule['sites']):
         if re.compile(rule['regex']).search(title):
           try:
-            if getattr(FindSpam, "%s" % rule['validation_method'])(title):
+            if getattr(TestClass, "%s" % rule['validation_method'])(title):
               result.append(rule['reason'])
           except KeyError:        # There is no special logic for this rule
             result.append(rule['reason'])
-    return result
-
-  @staticmethod
-  def testtitle(title):
-    regexes=["\\b(baba(ji)?|vashikaran|fashion|here is|porn)\\b","\\+\\d{10}","\\+?\\d{2}\\s?\\d{8}","\\b(asshole|crap|fag|fuck|idiot|shit|whore)s?\\b"]
-    result = []
-    p = [not not re.compile(s).search(title) for s in regexes]
-    if 'vashikaran' in title or 'baba' in title or True in p:
-      result.append('Possible spam')
-      # magic if matches word
     return result
 
   @staticmethod
@@ -41,7 +32,7 @@ class FindSpam:
     try:
       z = phonenumbers.parse(msg, "IN")
       if phonenumbers.is_possible_number(z) and phonenumbers.is_valid_number(z):
-        print "Possible %s, Valid %s, Expain: %s" % (phonenumbers.is_possible_number(z), phonenumbers.is_valid_number(z), z)
+        print "Possible %s, Valid %s, Explain: %s" % (phonenumbers.is_possible_number(z), phonenumbers.is_valid_number(z), z)
         return True
     except phonenumbers.phonenumberutil.NumberParseException:
       return False
