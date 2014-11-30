a5d0139f07eb416ea33e4901919e13cbaf2e9d1a
ProgramFOX <programfox@hotmail.be>
2014-11-30 18:20:40 +0100

    Use excepthook for threads

--- ws.py
+++ ws.py
@@ -35,6 +35,33 @@ def uncaught_exception(exctype, value, tb):
 
 sys.excepthook = uncaught_exception
 
+def installThreadExcepthook():
+    """
+    Workaround for sys.excepthook thread bug
+    From
+http://spyced.blogspot.com/2007/06/workaround-for-sysexcepthook-bug.html
+   
+(https://sourceforge.net/tracker/?func=detail&atid=105470&aid=1230540&group_id=5470).
+    Call once from __main__ before creating any threads.
+    If using psyco, call psyco.cannotcompile(threading.Thread.run)
+    since this replaces a new-style class method.
+    """
+    init_old = threading.Thread.__init__
+    def init(self, *args, **kwargs):
+        init_old(self, *args, **kwargs)
+        run_old = self.run
+        def run_with_except_hook(*args, **kw):
+            try:
+                run_old(*args, **kw)
+            except (KeyboardInterrupt, SystemExit):
+                raise
+            except:
+                sys.excepthook(*sys.exc_info())
+        self.run = run_with_except_hook
+    threading.Thread.__init__ = init
+
+installThreadExcepthook()
+
 class GlobalVars:
     false_positives = []
     whitelisted_users = []
