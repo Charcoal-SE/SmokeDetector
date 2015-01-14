738d064b06a4db5025b646cdb4dfd2eda2967291
ProgramFOX <programfox@hotmail.be>
2015-01-14 20:14:42 +0100

    Show build link when posting CI status

--- continuousintegration.py
+++ continuousintegration.py
@@ -3,6 +3,7 @@ import requests
 import re
 from globalvars import GlobalVars
 import datetime
+import json
 
 def watchCi():
     HOST = ''
@@ -27,6 +28,10 @@ def watchCi():
         print 'Received request from ' + addr[0] + " ; " + "verified as CircleCI" if is_circleci else "NOT verified as CircleCI!"
         if not is_circleci:
             continue
+        circleci_json = json.loads(s.recv(4096))
+        if not "payload" in circleci_json:
+            continue
+        build_url = circleci_json["payload"]["build_url"]
         r=requests.get('https://api.github.com/repos/Charcoal-SE/SmokeDetector/git/refs/heads/master')
         latest_sha = r.json()["object"]["sha"]
         r = requests.get('https://api.github.com/repos/Charcoal-SE/SmokeDetector/commits/' + latest_sha + '/statuses')
@@ -34,10 +39,10 @@ def watchCi():
             state = status["state"]
             if state == "success":
                 if datetime.datetime.strptime(status["updated_at"], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now()-datetime.timedelta(seconds=10):
-                    GlobalVars.charcoal_hq.send_message("CI build passed. Ready to pull!")
+                    GlobalVars.charcoal_hq.send_message("[CI build passed](%s). Ready to pull!" % build_url)
                     continue
             elif state == "error" or state == "failure":
                 if datetime.datetime.strptime(status["updated_at"], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now()-datetime.timedelta(seconds=10):
-                    GlobalVars.charcoal_hq.send_message("CI build failed, *someone* (prolly Undo) borked something!")
+                    GlobalVars.charcoal_hq.send_message("[CI build failed](%s), *someone* (prolly Undo) borked something!" % build_url)
                     continue
     s.close()
