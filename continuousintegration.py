5cb57d924e9f47360a8da365d7544fe3a1407e22
ProgramFOX <programfox@hotmail.be>
2015-01-14 20:19:02 +0100

    Fix link to build in CI status message

--- continuousintegration.py
+++ continuousintegration.py
@@ -28,21 +28,18 @@ def watchCi():
         print 'Received request from ' + addr[0] + " ; " + "verified as CircleCI" if is_circleci else "NOT verified as CircleCI!"
         if not is_circleci:
             continue
-        circleci_json = json.loads(s.recv(4096))
-        if not "payload" in circleci_json:
-            continue
-        build_url = circleci_json["payload"]["build_url"]
-        r=requests.get('https://api.github.com/repos/Charcoal-SE/SmokeDetector/git/refs/heads/master')
+        r = requests.get('https://api.github.com/repos/Charcoal-SE/SmokeDetector/git/refs/heads/master')
         latest_sha = r.json()["object"]["sha"]
         r = requests.get('https://api.github.com/repos/Charcoal-SE/SmokeDetector/commits/' + latest_sha + '/statuses')
         for status in r.json():
             state = status["state"]
+            target_url = status["target_url"]
             if state == "success":
                 if datetime.datetime.strptime(status["updated_at"], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now()-datetime.timedelta(seconds=10):
-                    GlobalVars.charcoal_hq.send_message("[CI build passed](%s). Ready to pull!" % build_url)
+                    GlobalVars.charcoal_hq.send_message("[CI build passed](%s). Ready to pull!" % target_url)
                     continue
             elif state == "error" or state == "failure":
                 if datetime.datetime.strptime(status["updated_at"], '%Y-%m-%dT%H:%M:%SZ') > datetime.datetime.now()-datetime.timedelta(seconds=10):
-                    GlobalVars.charcoal_hq.send_message("[CI build failed](%s), *someone* (prolly Undo) borked something!" % build_url)
+                    GlobalVars.charcoal_hq.send_message("[CI build failed](%s), *someone* (prolly Undo) borked something!" % target_url)
                     continue
     s.close()
