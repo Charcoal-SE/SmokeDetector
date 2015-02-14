b261d3556f416d2096db133389610072d2afadec
Parker Erway <Undo1@users.noreply.github.com>
2015-02-14 07:55:50 -0700

    test ci

--- ws.py
+++ ws.py
@@ -21,7 +21,6 @@ import time
 # won't be caught if they're not in a try-except block.
 # Hence, please avoid adding code before this comment, but if it's necessary,
 # test it thoroughly.
-# yahooooooooo to test the CI socket
 
 sys.excepthook = uncaught_exception
 install_thread_excepthook()
