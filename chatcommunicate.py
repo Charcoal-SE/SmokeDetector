94a2139d9a673ca9859f460ec49e7e8600f3b4a3
hichris1234 <christopherncarmel@hotmail.com>
2015-03-06 16:05:24 -0500

    Fix pluralization "bug"

--- chatcommunicate.py
+++ chatcommunicate.py
@@ -204,8 +204,9 @@ def watcher(ev, wrap2):
     if content_lower.startswith("!!/status"):
             now = datetime.utcnow()
             diff = now - UtcDate.startup_utc_date
-            hours, remainder = divmod(diff.seconds, 60)
-            ev.message.reply('Running since %s UTC (%s minutes)' % (GlobalVars.startup_utc, hours))
+            minutes, remainder = divmod(diff.seconds, 60)
+            minutestr = "minutes" if minutes != 1 else "minute"
+            ev.message.reply('Running since %s UTC (%s %s)' % (GlobalVars.startup_utc, minutes, minutestr))
     if content_lower.startswith("!!/reboot"):
         if is_privileged(ev_room, ev_user_id):
             post_message_in_room(ev_room, "Goodbye, cruel world")
