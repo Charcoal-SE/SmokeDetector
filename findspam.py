f366c4b61a9ab6baf50f0742ff512941792c6972
ProgramFOX <programfox@hotmail.be>
2015-03-11 18:55:21 +0100

    No tabs

--- findspam.py
+++ findspam.py
@@ -36,7 +36,7 @@ class FindSpam:
                             "svelmeblog", "preply\\.com", "hellofifa",
                             "fifa15online", "wearepropeople.com", "tagwitty",
                             "axsoccertours", "ragednablog", "ios8easyjailbreak",
-							"totalfitnesspoint"]
+                            "totalfitnesspoint"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|ಌ" % "|".join(bad_keywords), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
