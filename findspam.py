be2698e0c1ab05d8fc8b411d34e7848b4a4e9cbd
ProgramFOX <programfox@hotmail.be>
2015-03-11 18:51:37 +0100

    +totalfitnesspoint. --autopull

--- findspam.py
+++ findspam.py
@@ -35,7 +35,8 @@ class FindSpam:
                             "strongmenmuscle", "sh\\.st/", "musclehealthfitness",
                             "svelmeblog", "preply\\.com", "hellofifa",
                             "fifa15online", "wearepropeople.com", "tagwitty",
-                            "axsoccertours", "ragednablog", "ios8easyjailbreak"]
+                            "axsoccertours", "ragednablog", "ios8easyjailbreak",
+							"totalfitnesspoint"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|ಌ" % "|".join(bad_keywords), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
