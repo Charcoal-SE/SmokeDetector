6573b1e0369ea8338ab014eee4aafa4ca7f0258b
AWegnerGitHub <andy@andrewwegner.com>
2015-04-04 00:52:19 -0500

    Adds a new spam URL to blacklist that was common in March

--- findspam.py
+++ findspam.py
@@ -54,7 +54,8 @@ class FindSpam:
                             "recoverytoolbox\\.com", "sofotex\\.com",
                             "mybloggingmoney\\.com", "windows-techsupport\\.com",
                             "supplementsdeal\\.com", "drivethelife\\.com", "testolimitfacts\\.com",
-                            "lafozi\\.com", "hipslimgarcinia\\.com", "open-swiss-bank\\.com"]
+                            "lafozi\\.com", "hipslimgarcinia\\.com", "open-swiss-bank\\.com",
+							"originalgarciniacambogiafacts",]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
