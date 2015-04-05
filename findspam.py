6a3bf290a92728a0734aaf6c524c0533c7880cdd
hichris1234 <christopherncarmel@hotmail.com>
2015-04-04 22:07:31 -0400

    +crazybulkreviewsz

--- findspam.py
+++ findspam.py
@@ -79,7 +79,7 @@ class FindSpam:
                             "free-download-youtube\\.com", "free-music-downloader\\.com",
                             "video-download-capture\\.com", "videograbber\\.net",
                             "recoverwindowspassword\\.com", "password-buster\\.com",
-                            "remorecover\\.com", "remosoftware\\.com"]
+                            "remorecover\\.com", "remosoftware\\.com", "crazybulkreviewsz\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
