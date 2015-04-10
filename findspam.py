f74f19af651d94e94d1691a459d81b594abf0cc0
ProgramFOX <programfox@hotmail.be>
2015-04-10 14:32:56 +0200

    +listoffreeware.com

--- findspam.py
+++ findspam.py
@@ -84,7 +84,8 @@ class FindSpam:
                             "patch\\.com", "ajgilworld\\.com", "santomais", "viilms",
                             "clashofclansastucegemmes\\.com", "mothersday-2014\\.org",
                             "bestcelebritiesvideo\\.com", "shopnhlbruins\\.com",
-                            "repairpstrecovery\\.com", "downloadscanpst\\.com"]
+                            "repairpstrecovery\\.com", "downloadscanpst\\.com",
+                            "listoffreeware\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
