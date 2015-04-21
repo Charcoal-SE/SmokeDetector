30070f17a86ae54d9799ef9f6182db1d6429a1bc
hichris1234 <christopherncarmel@hotmail.com>
2015-04-21 19:34:41 -0400

    +musclebuildingproducts

--- findspam.py
+++ findspam.py
@@ -89,7 +89,8 @@ class FindSpam:
                             "bestcelebritiesvideo\\.com", "shopnhlbruins\\.com",
                             "repairpstrecovery\\.com", "downloadscanpst\\.com",
                             "listoffreeware\\.com", "bigasoft\\.com", "opclub07\\.com",
-                            "allavsoft", "vpnranks\\.com", "garciniasecretdietabout\\.com"]
+                            "allavsoft", "vpnranks\\.com", "garciniasecretdietabout\\.com",
+                            "musclebuildingproducts\\.info"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
