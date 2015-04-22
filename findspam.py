86cf7b23019d95c73b6756cc9c28aefc8bbfab45
ProgramFOX <programfox@hotmail.be>
2015-04-22 14:23:48 +0200

    Add some blacklisted websites

--- findspam.py
+++ findspam.py
@@ -90,7 +90,8 @@ class FindSpam:
                             "repairpstrecovery\\.com", "downloadscanpst\\.com",
                             "listoffreeware\\.com", "bigasoft\\.com", "opclub07\\.com",
                             "allavsoft", "vpnranks\\.com", "garciniasecretdietabout\\.com",
-                            "musclebuildingproducts\\.info", "magichealthandwellness\\.com"]
+                            "musclebuildingproducts\\.info", "magichealthandwellness\\.com",
+                            "vanskeys\\.com", "cheapessaywritingservice", "edbtopsts\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
