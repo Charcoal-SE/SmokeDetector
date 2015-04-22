12d0d347794a86f267f9085ea8647da8c5553e78
ProgramFOX <programfox@hotmail.be>
2015-04-22 14:20:16 +0200

    +magichealthandwellness.com

--- findspam.py
+++ findspam.py
@@ -90,7 +90,7 @@ class FindSpam:
                             "repairpstrecovery\\.com", "downloadscanpst\\.com",
                             "listoffreeware\\.com", "bigasoft\\.com", "opclub07\\.com",
                             "allavsoft", "vpnranks\\.com", "garciniasecretdietabout\\.com",
-                            "musclebuildingproducts\\.info"]
+                            "musclebuildingproducts\\.info", "magichealthandwellness\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
