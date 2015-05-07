6838d8e65e1c0699fcb654813e32d0d2c0e05a56
ProgramFOX <programfox@hotmail.be>
2015-05-07 17:08:38 +0200

    +couchsurfing

--- findspam.py
+++ findspam.py
@@ -99,7 +99,7 @@ class FindSpam:
                             "vpnfaqs\\.com", "vpnanswers\\.com", "bestcheapvpnservice\\.com",
                             "unblockingtwitter\\.com", "openingblockedsite\\.com",
                             "arabicdownloads\\.com", "arabicsoftdownload\\.com",
-                            "repairtoolbox\\.com"]
+                            "repairtoolbox\\.com", "couchsurfing\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
