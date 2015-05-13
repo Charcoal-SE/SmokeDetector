cc28c1136be71390a5732b048294a5fab22eaae2
hichris1234 <christopherncarmel@hotmail.com>
2015-05-12 20:03:37 -0400

    Really flake?

--- globalvars.py
+++ globalvars.py
@@ -75,7 +75,7 @@ class GlobalVars:
                                               "166899",  # Second Rikudo
                                               "287999",  # ASCIIThenANSI
                                               "208518",  # JNat
-                                              "284141"]}  # michaelpri 
+                                              "284141"]}  # michaelpri
     smokeDetector_user_id = {charcoal_room_id: "120914", meta_tavern_room_id: "266345"}
 
     censored_committer_names = {"3f4ed0f38df010ce300dba362fa63a62": "Undo1"}
