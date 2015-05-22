e1506df576f319362adb8e7b814a347a84081ed0
ProgramFOX <programfox@hotmail.be>
2015-05-22 16:31:21 +0200

    Add *facts.(com|org|net) to blacklisted websites

--- findspam.py
+++ findspam.py
@@ -106,7 +106,8 @@ class FindSpam:
                             "tophealthresource\\.com", "cloudinsights\\.net",
                             "(premium|priceless)-inkjet\\.com", "antivirus\\.comodo\\.com",
                             "clusterlinks\\.com", "connectify\\.me", "liftserump\\.com",
-                            "freeiphonedatarecovery\\.com", "kizi1000\\.in", "weightruinations\\.com"]
+                            "freeiphonedatarecovery\\.com", "kizi1000\\.in", "weightruinations\\.com",
+                            "facts\\.(com|net|org)"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
