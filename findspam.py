292d9511ca25b8ae5c5793d66574cf35c9214154
ProgramFOX <programfox@hotmail.be>
2015-05-25 11:58:04 +0200

    Add missing comma
    This did not cause any errors, but it did concatenate two spam keywords.

--- findspam.py
+++ findspam.py
@@ -16,7 +16,7 @@ class FindSpam:
                     "Xtreme Antler", "Maxx Test 3000", "orvigomax",
                     "Cheap Wigs?", "jivam", "(Improve )?Brain Power", "Maximum ?Shred",
                     "aging skin", "acne( prone)? skin", "(skin )?eye serum",
-                    "skin (serum|eye)", "bagprada", "6611165613" "Apowersoft",
+                    "skin (serum|eye)", "bagprada", "6611165613", "Apowersoft",
                     "Service Solahart", "junisse",
                     "(fake|original|uk|novelty) (passports?|driver'?s? licen[cs]e|ID cards?)",
                     "(support|service|helpline)( phone)? number|1[ -]?[ -]?[ -]?866[ -]?978[ -]?(6819|6762)",
