601c61e7ee8d5215c1c8d91f0efec80186528cdf
AWegnerGitHub <andy@andrewwegner.com>
2015-05-28 22:07:41 -0500

    Fixes spaces/tabs mixup.

--- findspam.py
+++ findspam.py
@@ -111,8 +111,8 @@ class FindSpam:
                             "facts\\.(com|net|org)", "products\\.odosta\\.com",
                             "rackons\\.com", "imonitorsoft\\.com",
                             "analec\\.com",
-							"gojivitafacts\\.com", "dermaessencecreamblog\\.com", "stadtbett\\.com", 
-							"eyeluminousfacts\\.com", "healthcaresdiscussion\\.com"]
+                            "gojivitafacts\\.com", "dermaessencecreamblog\\.com", "stadtbett\\.com", 
+                            "eyeluminousfacts\\.com", "healthcaresdiscussion\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
