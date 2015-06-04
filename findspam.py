c305fcbc4dbb7f05c2bcbaac69fde891603c1c86
ProgramFOX <programfox@hotmail.be>
2015-06-04 17:22:09 +0200

    Remove duplicate regex

--- findspam.py
+++ findspam.py
@@ -115,7 +115,7 @@ class FindSpam:
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
-        {'regex': u"(?i)\\b(baba|nike)\\b|\\p{Script=Hangul}", 'all': True,
+        {'regex': u"(?i)\\b(baba|nike)\\b", 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': False, 'stripcodeblocks': False, 'body_summary': True},
         {'regex': u"(?i)\\p{Script=Hangul}", 'all': True,
          'sites': [], 'reason': "Korean character in {}", 'title': True, 'body': True, 'username': False, 'stripcodeblocks': False, 'body_summary': True},
