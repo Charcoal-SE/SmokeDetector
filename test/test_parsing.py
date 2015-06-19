406e78167622208bb63bc4953dd31636bcd5eec3
ProgramFOX <programfox@hotmail.be>
2015-06-19 12:58:38 +0200

    Do some debugging about Circle's test failures

--- test/test_parsing.py
+++ test/test_parsing.py
@@ -4,6 +4,7 @@ import pytest
 test_data_inputs = []
 with open("test/data_test_parsing.txt", "r") as f:
     test_data_inputs = f.readlines()
+print(len(test_data_inputs))
 
 # Large inputs should go to that file.
 # Only inputs should go there, not the parsing method and the expected output,
