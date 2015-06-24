c37512c119d4245e1c696d6743dc7135c29aa2a9
hichris1234 <christopherncarmel@hotmail.com>
2015-06-24 17:29:12 -0400

    +best property management

--- findspam.py
+++ findspam.py
@@ -28,7 +28,7 @@ class FindSpam:
                     r"(?x:B [\s_]* A [\s_]* M \W{0,5} W [\s_]* A [\s_]* R [\s_]* \.? [\s_]* C [\s_]* O [\s_]* M)",
                     "slumber pm", "1-844-400-7325", "male enhancement", "bestcollegechina",
                     "bbwdesire", "rsorder", "Shopping ?Cart ?Elite", "Easy ?Data ?Feed",
-                    "breasts? enlargement"]
+                    "breasts? enlargement", "best property management"]
     bad_keywords_nwb = [u"ಌ", "babyliss", "garcinia", "acai ?berry",  # "nwb" == "no word boundary"
                         "aging ?cream", "b ?a ?m ?((w ?o ?w)|(w ?a ?r))",
                         "abam26", "watch2live"]
