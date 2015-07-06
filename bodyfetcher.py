f444e47623e950a77cd7a029438fb743ee7e1a13
hichris1234 <christopherncarmel@hotmail.com>
2015-07-06 19:03:36 -0400

    Bump math to 10 questions before request, bump SU down to 2

--- bodyfetcher.py
+++ bodyfetcher.py
@@ -11,8 +11,7 @@ class BodyFetcher:
 
     specialCases = {"stackoverflow.com": 5,
                     "serverfault.com": 5,
-                    "superuser.com": 5,
-                    "math.stackexchange.com": 5,
+                    "math.stackexchange.com": 10,
                     "drupal.stackexchange.com": 1,
                     "meta.stackexchange.com": 1}
 
