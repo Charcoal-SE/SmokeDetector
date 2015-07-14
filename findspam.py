c0f4f134071d9fb6b3bcb133203ff19eb08229f3
normalhuman <leonid.traveling@gmail.com>
2015-07-14 06:39:38 +0000

    health+pro = spam

--- findspam.py
+++ findspam.py
@@ -158,7 +158,11 @@ class FindSpam:
                             "cacherealestate\\.com", "Matrixhackka007", "aoatech\\.com",
                             "pharaohtools", "msoutlooktools\\.com", "softwarezee",
                             "i-hire\\.pro", "pandamw\\.com", "buy[\\w-]{6,}\\.(com|net|org)",
-                            "(testo|cleanse|supplement|serum)[\\w-]*\\.(com|net|org)"]
+                            "(testo|cleanse|supplement|serum|fatloss)[\\w-]*\\.(com|net|org)",
+                            "(natural|pro|magic)[\\w-]*health[\\w-]*\\.(com|net|org)",
+                            "health[\\w-]*(natural|pro|review|blog|advise|discussion)[\\w-]*\\.(com|net|org)",
+                            "scampunch\\.com", "multipelife\\.com", "seasoncars\\.com",
+                            "eltima\\.com", "flexihub\\.com"]
     rules = [
         {'regex': u"(?i)\\b(%s)\\b|%s" % ("|".join(bad_keywords), "|".join(bad_keywords_nwb)), 'all': True,
          'sites': [], 'reason': "Bad keyword in {}", 'title': True, 'body': True, 'username': True, 'stripcodeblocks': False, 'body_summary': True},
