8d7a3be9e7bee36df3b1024ca6ea2c4f3d0095a2
ProgramFOX <programfox@hotmail.be>
2015-07-21 10:33:44 +0200

    Don't return there

--- chatcommunicate.py
+++ chatcommunicate.py
@@ -381,7 +381,7 @@ def handle_commands(content_lower, message_parts, ev_room, ev_user_id, ev_user_n
         if len(reasons_body) > 0:
             result += "Body: blacklisted: " + ", ".join(reasons_body).capitalize() + " ; "
         else:
-            return "Body: not blacklisted ; "
+            result += "Body: not blacklisted ; "
         reasons_title = FindSpam.test_post(string_to_test, "", "", "", False, False)
         if len(reasons_title) > 0:
             result += "Title: blacklisted: " + ", ".join(reasons_body).capitalize()
