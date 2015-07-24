812f5b2cda44a4ed240848d280b96e273bdb498f
Normal Human <normalhuman@users.noreply.github.com>
2015-07-24 00:19:29 -0400

    +ProBrain, {long}advice domain

--- findspam.py
+++ findspam.py
@@ -41,7 +41,7 @@ class FindSpam:
                     "fifaodell", "brianfo", "tosterone", "bajotz",
                     "vashi?k[ae]r[ae]n", "kolcak", "Zapyo", "we (offer|give out) loans",
                     "porn", "molvi", "judi bola", "ituBola.com", "lost lover'?s?",
-                    "acai", "rejuvenated skin",
+                    "acai", "rejuvenated skin", "ProBrain",
                     "LifeForce", "swtor2credits", "me2.do", "black magic",
                     "bam2u", "Neuro(3X|flexyn|fuse|luma|plex)", "Nutra", "TesteroneXL", "Nitroxin",
                     "Bowtrol", "Slim ?Genix", "Cleanse EFX", "Alpha Rush",
@@ -166,7 +166,8 @@ class FindSpam:
                             "scampunch\\.com", "multipelife\\.com", "seasoncars\\.com",
                             "eltima\\.com", "flexihub\\.com", "\\.debt\\.com",
                             "hotfrog\\.ca", "snorg(content|tees)\\.com", "webtechcoupons",
-                            "(\\.|best)cheap[\\w-]+\\.co", "[\\w-]{10,}(sale|reviews?)\\.co", "architecturedesign\\.tk",
+                            "(\\.|best)cheap[\\w-]+\\.co", "[\\w-]{10,}(sale|reviews?|advices?)\\.co",
+                            "architecturedesign\\.tk",
                             "fat(burn|loss)[\\w-]*\\.(co|net|org|info)", "(corrupt|repair)[\\w-]*.blogspot",
                             "xinyanlaw", "ultrafinessesite", "sunitlabs\\.com", "puravol\\.net",
                             "statesmovie"]
