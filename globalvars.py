6486ad2e0620c0064b867e3679e0d4ad82f53f6c
ProgramFOX <programfox@hotmail.be>
2015-08-15 14:00:07 +0200

    Add S.L. Barth to the privileged Tavern users

--- globalvars.py
+++ globalvars.py
@@ -84,7 +84,8 @@ class GlobalVars:
                                               "152859",  # Shadow Wizard
                                               "201314",  # apnorton
                                               "280934",  # M.A.Ramezani
-                                              "200235"],  # durron597
+                                              "200235",  # durron597
+                                              "168333"]  # S.L. Barth
                         socvr_room_id: [
                             "1849664",  # Undo
                             "2581872",  # hichris123
