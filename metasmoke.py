b2186357b04c2b0b8ef97cf566f65abeca0efae1
Undo1 <parker@erwaysoftware.com>
2015-08-18 21:54:03 -0400

    take title instead

--- metasmoke.py
+++ metasmoke.py
@@ -4,9 +4,9 @@ import requests
 
 class Metasmoke:
     @classmethod
-    def send_stats_on_post(self, post, reasons):
+    def send_stats_on_post(self, title, reasons):
         
-        payload = { 'post' : { 'title' : 'python', 'reasons': reasons } }
+        payload = { 'post' : { 'title' : title, 'reasons': reasons } }
 
         headers = {'Content-type': 'application/json'}
         r=requests.post("http://localhost:3000/posts.json", data=json.dumps(payload), headers=headers)
