ab1d78ee5a1f9b15f08ce50d06d53a1d58b8628e
Undo1 <parker@erwaysoftware.com>
2015-08-23 17:35:19 -0600

    don't crash if metasmoke isn't running or returns an error or something

--- metasmoke.py
+++ metasmoke.py
@@ -5,8 +5,10 @@ import requests
 class Metasmoke:
     @classmethod
     def send_stats_on_post(self, title, reasons):
+        try:
+            payload = {'post': {'title': title, 'reasons': reasons}}
 
-        payload = {'post': {'title': title, 'reasons': reasons}}
-
-        headers = {'Content-type': 'application/json'}
-        requests.post("http://localhost:3000/posts.json", data=json.dumps(payload), headers=headers)
+            headers = {'Content-type': 'application/json'}
+            requests.post("http://localhost:3000/posts.json", data=json.dumps(payload), headers=headers)
+        except Exception as e:
+            print e
