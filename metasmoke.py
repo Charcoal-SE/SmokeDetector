198ed7e2a7d44ed6d26151f9ce51827a0ffc56cc
Undo1 <parker@erwaysoftware.com>
2015-08-29 19:33:15 -0600

    details, details --autopull

--- metasmoke.py
+++ metasmoke.py
@@ -28,7 +28,7 @@ class Metasmoke:
             payload = {'feedback': {'user_name': user_name, 'feedback_type': feedback_type, 'post_link': post_link}}
 
             headers = {'Content-type': 'application/json'}
-            requests.posts(GlobalVars.metasmoke_host + "/feedbacks.json", data=json.dumps(payload), headers=headers)
+            requests.post(GlobalVars.metasmoke_host + "/feedbacks.json", data=json.dumps(payload), headers=headers)
 
         except Exception as e:
             print e
