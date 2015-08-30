69e1539aa77c016304afecc2026e019d9353da2c
ProgramFOX <programfox@hotmail.be>
2015-08-30 15:14:13 +0200

    Filter for !!/report (answer) should include link

--- apigetpost.py
+++ apigetpost.py
@@ -21,7 +21,7 @@ def api_get_post(post_url):
         return None
     post_id, site, post_type = d
     if post_type == "answer":
-        api_filter = "!SYD2Oot8YGufUM6hpT"
+        api_filter = "!1zsjS8Aa91CUeucj0DTmP"
         req_url = "http://api.stackexchange.com/2.2/answers/" + post_id +\
                   "?site=" + site + "&filter=" + api_filter +\
                   "&key=IAkbitmze4B8KpacUfLqkw(("
