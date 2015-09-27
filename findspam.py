be73630cfcd4985fc0ce810aa853e48a36782aba
Undo1 <Undo1@users.noreply.github.com>
2015-09-27 14:39:59 -0600

    +guyideas.esy.es --autopull

--- findspam.py
+++ findspam.py
@@ -198,7 +198,7 @@ class FindSpam:
                             "pdfask\\.com", "bookbrokerz\\.com", "solarismovies", "giftsinmind\\.co",
                             "eximiussoftsolutions\\.com", "droid4apk\\.", "canwestcellular",
                             "pages\\.rediff\\.com", "limitlesspill", "eltib2\\.wordpress\\.com",
-                            "access-electronic\\.co"]
+                            "access-electronic\\.co", "guyideas\\.esy\\.es"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/xtra[\w-]", r"//xtra[\w-]*?\.(co|net|org|in\W|info)",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
