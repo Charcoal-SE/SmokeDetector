7808983c9e9e36b5942fcd60abcd0ab227e7ba4d
Undo1 <Undo1@users.noreply.github.com>
2015-10-15 08:03:51 -0600

    Add Paul Roub to privileged users

--- globalvars.py
+++ globalvars.py
@@ -119,7 +119,8 @@ class GlobalVars:
                             "3933332",  # Rizier123
                             "2422013",  # cybermonkey
                             "3478852",  # Nisse Engström
-                            "2302862"]  # Siguza
+                            "2302862",  # Siguza
+                            "1324"] #  Paul Roub
                         }
     smokeDetector_user_id = {charcoal_room_id: "120914", meta_tavern_room_id: "266345",
                              socvr_room_id: "3735529"}
