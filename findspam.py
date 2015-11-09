8696b325a866dfe6b09b053e8bc297d17dc64d8b
Normal Human <normalhuman@users.noreply.github.com>
2015-11-09 01:22:11 -0500

    +jihosoft

--- findspam.py
+++ findspam.py
@@ -196,7 +196,7 @@ class FindSpam:
                             "[./]occn\\.org", "illusiongroups\\.com", "varite\\.com", "hooraysoft\\.com",
                             "gcbxnow\\.com", "godowell\\.net", "place4papers", "tradingqna\\.com",
                             "shacamerica\\.net", "nillowpages\\.com", "letsnurture\\.com", "healthpeters\\.com",
-                            "rozapk\\.com"]
+                            "rozapk\\.com", "jihosoft\\.com"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/xtra[\w-]", r"//xtra[\w-]*?\.(co|net|org|in\W|info)",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
