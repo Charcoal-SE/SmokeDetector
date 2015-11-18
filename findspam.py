b8b60ef97d0920daeb388b55a25787fea6bde7c7
rschrieken <rene.schrieken@gmail.com>
2015-11-18 22:56:42 +0100

    black listed *.technical-care.com.
    http://chat.meta.stackexchange.com/transcript/message/4215801#4215801

--- findspam.py
+++ findspam.py
@@ -204,7 +204,7 @@ class FindSpam:
                             "[./]occn\\.org", "illusiongroups\\.com", "varite\\.com", "hooraysoft\\.com",
                             "gcbxnow\\.com", "godowell\\.net", "place4papers", "tradingqna\\.com",
                             "shacamerica\\.net", "nillowpages\\.com", "letsnurture\\.com", "healthpeters\\.com",
-                            "rozapk\\.com", "jihosoft\\.com", "mahnazmezon\\.com"]
+                            "rozapk\\.com", "jihosoft\\.com", "mahnazmezon\\.com", "technical-care\\.com"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/xtra[\w-]", r"//xtra[\w-]*?\.(co|net|org|in\W|info)",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
