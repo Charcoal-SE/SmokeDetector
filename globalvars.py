77f5d072480bb83ba20726ab8faa0e1c1d132f5b
rschrieken <rene.schrieken@gmail.com>
2015-11-24 10:47:27 +0100

    added triplee to socvr users

--- globalvars.py
+++ globalvars.py
@@ -131,7 +131,8 @@ class GlobalVars:
                             "4174897",  # Kyll
                             "5299236",  # Kevin Guan
                             "4050842",  # Thaillie
-                            "1816093"]  # Drew
+                            "1816093",  # Drew
+                            "874188"]  # Triplee
                         }
     smokeDetector_user_id = {charcoal_room_id: "120914", meta_tavern_room_id: "266345",
                              socvr_room_id: "3735529"}
