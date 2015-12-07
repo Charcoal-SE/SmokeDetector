f5618d40ee33e64c033be26ece328fccffd38a95
Undo1 <Undo1@users.noreply.github.com>
2015-12-06 22:13:05 -0700

    +adonads.com

--- findspam.py
+++ findspam.py
@@ -252,7 +252,8 @@ class FindSpam:
                             "skyformation\\.com", "shiftingsolutions\\.in", "bandsawjudge\\.com",
                             "liveestorebuilder\\.com", "exampracticequestions\\.com", "createspace\\.com",
                             "healthpeters\\.com", "fun-flicks\\.com", "smarketingclub\\.com", "cbitss\\.in",
-                            "o-lovius\\.com", "crackedapkfull\\.com", "aldovmcgregor\\.co", "priredeream\\.com"]
+                            "o-lovius\\.com", "crackedapkfull\\.com", "aldovmcgregor\\.co", "priredeream\\.com",
+                            "adonads\\.com"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/(xtra|muscle)[\w-]", r"//xtra[\w-]*?\.(co|net|org|in\W|info)",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
