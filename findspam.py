f1c1ad69e715100a2ec64ec8ca00d6010818d9d0
Normal Human <normalhuman@users.noreply.github.com>
2015-12-27 12:04:46 -0500

    + j.gs

--- findspam.py
+++ findspam.py
@@ -176,7 +176,7 @@ class FindSpam:
                             "funmac", "lovebiscuits", "z-data.blogspot.com",
                             "Eglobalfitness", "musclezx90site", "fifapal",
                             "hits4slim", "screenshot\\.net", "downloadavideo\\.net",
-                            "strongmenmuscle", "sh\\.st/", "adf\\.ly/", "musclehealthfitness",
+                            "strongmenmuscle", "sh\\.st/", "adf\\.ly/", "//j\\.gs/", "musclehealthfitness",
                             "preply\\.com", "hellofifa", "chinadivision",
                             "fifa\\d*online", "wearepropeople.com", "tagwitty",
                             "axsoccertours", "ragednablog", "ios8easyjailbreak",
