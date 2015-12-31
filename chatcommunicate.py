86443c96451a6e0a9a1ee9df2efef3772edeec73
hichris1234 <christopherncarmel@hotmail.com>
2015-12-31 12:45:14 -0500

    Hopefully fix one unicode -> ASCII error
    !!/blame didn't return a unicode string

--- chatcommunicate.py
+++ chatcommunicate.py
@@ -476,7 +476,7 @@ def handle_commands(content_lower, message_parts, ev_room, ev_room_name, ev_user
     if content_lower.startswith("!!/blame"):
         GlobalVars.users_chatting[ev_room] = list(set(GlobalVars.users_chatting[ev_room]))  # Make unique
         user_to_blame = random.choice(GlobalVars.users_chatting[ev_room])
-        return "It's [{}]({})'s fault.".format(user_to_blame[0], user_to_blame[1])
+        return u"It's [{}]({})'s fault.".format(user_to_blame[0], user_to_blame[1])
     if "smokedetector" in content_lower and "fault" in content_lower and ("xkcdbot" in ev_user_name.lower() or "bjb568" in ev_user_name.lower()):
         return "Liar"
     if content_lower.startswith("!!/coffee"):
