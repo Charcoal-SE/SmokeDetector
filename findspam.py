ab739ec0929deb749c788a1b953dd11af6520201
ProgramFOX <programfox@hotmail.be>
2016-01-04 17:20:53 +0100

    Add a few blacklisted sites

--- findspam.py
+++ findspam.py
@@ -297,7 +297,8 @@ class FindSpam:
                             "whatech\\.com", "crunchbase\\.com", "fileniaz\\.com", "icoolsoft\\.com",
                             "replicabreitlingwatches\\.co", "wonderful-watch\\.co", "plagiarism-checker\\.me",
                             "khannaescorts\\.com", "windowslivemailconverter\\.com", "mboxconverter\\.com",
-                            "sapboonline\\.com"]
+                            "sapboonline\\.com", "thinkittraining\\.in", "salesforcetrainingexpert\\.in",
+                            "weblogic-trainingchennai\\.in"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/(xtra|muscle)[\w-]",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
