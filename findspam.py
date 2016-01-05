3d8e9c440d995edde3a478d749538a300ca81345
Normal Human <normalhuman@users.noreply.github.com>
2016-01-05 12:24:23 -0500

    deobfuscation and key phrase for support #

--- findspam.py
+++ findspam.py
@@ -92,7 +92,11 @@ def has_phone_number(s, site):
 
 def has_customer_service(s, site):  # flexible detection of customer service in titles
     s = s[0:200].lower()   # if applied to body, the beginning should be enough: otherwise many false positives
-    business = regex.compile(r"(?i)\b(dell|epson|facebook|gmail|google|hotmail|hp|lexmark|mcafee|microsoft|out[l1]ook|quickbooks|yahoo|Delta|airlines?)\b").findall(s)
+    s = regex.sub("[^\w\s]", "", s)   # deobfuscate
+    phrase = regex.compile(r"(support|service|contact|help(line)?)( telephone|phone)? number", s).findall(s)
+    if (phrase):
+        return True, "Keywords: {}".format(phrase[0])
+    business = regex.compile(r"(?i)\b(netflix|dell|epson|facebook|gmail|google|hotmail|hp|lexmark|mcafee|microsoft|out[l1]ook|quickbooks|yahoo|Delta|airlines?)\b").findall(s)
     digits = len(regex.compile(r"\d").findall(s))
     if (business and digits >= 5):
         keywords = regex.compile(r"(?i)\b(customer|help|helpline|reservation|phone|recovery|service|support|tech|technical|telephone|number)\b").findall(s)
