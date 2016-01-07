f54a60013ef49a7c6f6c6fbb31741c110568e2b6
Normal Human <normalhuman@users.noreply.github.com>
2016-01-07 01:45:15 -0500

    +achatlaser

--- findspam.py
+++ findspam.py
@@ -304,7 +304,7 @@ class FindSpam:
                             "replicabreitlingwatches\\.co", "wonderful-watch\\.co", "plagiarism-checker\\.me",
                             "khannaescorts\\.com", "windowslivemailconverter\\.com", "mboxconverter\\.com",
                             "sapboonline\\.com", "thinkittraining\\.in", "salesforcetrainingexpert\\.in",
-                            "weblogic-trainingchennai\\.in", "indiaflower", "krojamcleaner\\.com"]
+                            "weblogic-trainingchennai\\.in", "indiaflower", "krojamcleaner\\.com", "achatlaser\\.com"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/(xtra|muscle)[\w-]",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
