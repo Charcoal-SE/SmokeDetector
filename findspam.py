31036fdcb5e3425452fc9871b3fc7b5e7a8203a1
hichris1234 <christopherncarmel@hotmail.com>
2016-01-14 21:00:00 -0500

    Fix unicode error

--- findspam.py
+++ findspam.py
@@ -177,7 +177,7 @@ class FindSpam:
                         "(brain|breast|male|penile|penis)[- ]?(enhance|enlarge|improve|boost|plus|peak)",
                         "renuva(cell|derm)", " %uh ", " %ah ", "svelme", "tapsi ?sarkar", "viktminskning",
                         "unique(doc)?producers", "green ?tone ?pro", "troxyphen", "seremolyn", "revolyn",
-                        "training.{0,4}chennai", "Ｃ[Ｏ|0]Ｍ"]
+                        "training.{0,4}chennai", u"Ｃ[Ｏ|0]Ｍ"]
     blacklisted_websites = ["online ?kelas", "careyourhealths", "wowtoes",
                             "ipubsoft", "orabank", "powerigfaustralia",
                             "cfpchampionship2015playofflive", "rankassured\\.com",
