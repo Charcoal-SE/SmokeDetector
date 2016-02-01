93f8c2a02daf4692129889a542c6fdae94ec38a8
Normal Human <normalhuman@users.noreply.github.com>
2016-01-31 20:23:28 -0500

    + routeperfect

--- findspam.py
+++ findspam.py
@@ -316,7 +316,7 @@ class FindSpam:
                             "trutech\\.co", "askmespam\\.com", "imdresses\\.com", "doesitscam\\.com",
                             "jobsopening\\.co\\.in", "androidappsformac\\.com", "retersweld\\.com", "mindextra\\.com",
                             "psychicfuguestudio", "softserialhq\\.com", "unstopableshrine\\.webs\\.com",
-                            "softaken\\.com", "lyonstechnologies", "serialkeygeneratorfree\\.com"]
+                            "softaken\\.com", "lyonstechnologies", "serialkeygeneratorfree\\.com", "routeperfect\\.com"]
     pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/(xtra|muscle)[\w-]", r"http\S*?\Wfor-sale\W",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys)\.com",
