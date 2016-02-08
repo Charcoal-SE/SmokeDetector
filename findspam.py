0580ad770045215c7e2cd1997817856e33ed2f37
Normal Human <normalhuman@users.noreply.github.com>
2016-02-07 19:54:08 -0500

    tighter health123 pattern

--- findspam.py
+++ findspam.py
@@ -320,7 +320,7 @@ class FindSpam:
                             "tupely\\.com", "apkheart\\.com", "uflysoft\\.net", "nimblemessaging\\.com",
                             "teksonit\\.com", "educba\\.com", "oleville\\.net", "nutribulletrecipes\\.org",
                             "decalontop\\.com", "urlopener\\.com", "mobile57\\.com"]
-    pattern_websites = [r"health\d{3,}", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
+    pattern_websites = [r"health\d{3,}\.(com|net)", r"http\S*?\.repair\W", r"filefix(er)?\.com", "\.page\.tl\W",
                         r"\.(com|net)/(xtra|muscle)[\w-]", r"http\S*?\Wfor-sale\W",
                         r"fifa\d+[\w-]*?\.com", r"[\w-](giveaway|jackets|supplys|male)\.com",
                         r"(essay|resume)\w{6,}\.(co|net|org|in\W|info|us)",
