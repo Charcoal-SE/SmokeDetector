01aa4e9b13080b4cc330b8e7732958e63f2a30a0
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2016-02-22 14:01:19 +0000

    Add Petter Friberg to SOCVR
    https://chat.stackoverflow.com/transcript/message/28889824#28889824

--- globalvars.py
+++ globalvars.py
@@ -169,7 +169,8 @@ class GlobalVars:
                             "5389107",  # Magisch
                             "4099593",  # bhargav-rao
                             "1542723",  # Ferrybig
-                            "2025923"]  # Tushar
+                            "2025923",  # Tushar
+                            "5292302"]  # Petter Friberg
                         }
     smokeDetector_user_id = {charcoal_room_id: "120914", meta_tavern_room_id: "266345",
                              socvr_room_id: "3735529"}
