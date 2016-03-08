d270c597d1c69dc3ebf88be0eca0176a5c691649
Normal Human <normalhuman@users.noreply.github.com>
2016-03-08 13:54:17 -0500

    BlackListHackers

--- findspam.py
+++ findspam.py
@@ -334,7 +334,7 @@ class FindSpam:
                             "vizayn\\.com", "health(flyup|buzzer)\\.com", "androidpureapk\\.com",
                             "upsafe\\.com", "spiritsofts\\.com", "rcptec\\.com", "gmax-brasil\\.com", "icognix\\.net",
                             "\Wpysoft\\.com", "zescode\\.com", "eserviceshelp\\.in", "captainform\\.com",
-                            "techiphone\\.com", "kmminoaq4yci5woj.onion", "ultraslimsystembrazil.net"]
+                            "techiphone\\.com", "kmminoaq4yci5woj\\.onion", "BlackListHackers\\.com"]
     # Patterns: the top few lines are the most straightforward, matching any site with this string in domain name
     pattern_websites = [r"(inboxmachine|(get|buy)cheap|escort|diploma|extramoney|earnathome|spell(caster|specialist)|profits|seo(tool|service|trick)|onsale|fat(burn|loss)|(\.|//|best)cheap|online(training|solution))[\w-]*?\.(co|net|org|in\W|info|wordpress|blogspot)",
                         r"(trainingin|cracked\w{3}|bestmover|relocation|\w{4}mortgage|loans|revenue|testo[-bsx]|cleanse|cleansing|detox|supplement|lubricant|serum|wrinkle|topcare|freetrial)[\w-]*?\.(co|net|org|in\W|info|wordpress|blogspot)",
