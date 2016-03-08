008d0f440939a543b67ee7add2be97ae5157422b
Undo1 <Undo1@users.noreply.github.com>
2016-03-07 17:52:27 -0700

    +kmminoaq4yci5woj.onion
    https://chat.stackexchange.com/transcript/message/28078721#28078721

--- findspam.py
+++ findspam.py
@@ -339,7 +339,7 @@ class FindSpam:
                             "vizayn\\.com", "health(flyup|buzzer)\\.com", "phenq\\.org", "androidpureapk\\.com",
                             "upsafe\\.com", "spiritsofts\\.com", "rcptec\\.com", "gmax-brasil\\.com", "icognix\\.net",
                             "\Wpysoft\\.com", "zescode\\.com", "eserviceshelp\\.in", "captainform\\.com",
-                            "techiphone\\.com"]
+                            "techiphone\\.com", "kmminoaq4yci5woj.onion"]
     # Patterns: the top few lines are the most straightforward, matching any site with this string in domain name
     pattern_websites = [r"(inboxmachine|(get|buy)cheap|escort|diploma|extramoney|earnathome|spell(caster|specialist)|profits|seo(tool|service|trick)|onsale|fat(burn|loss)|(\.|//|best)cheap|online(training|solution))[\w-]*?\.(co|net|org|in\W|info|wordpress|blogspot)",
                         r"(trainingin|cracked\w{3}|bestmover|relocation|\w{4}mortgage|loans|revenue|testo[-bsx]|cleanse|cleansing|detox|supplement|lubricant|serum|wrinkle|topcare|freetrial)[\w-]*?\.(co|net|org|in\W|info|wordpress|blogspot)",
