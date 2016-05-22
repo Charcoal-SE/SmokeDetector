1ec634824d6f65d3dd30bce30cba7f972c4957fc
Quill <seriouslyquill@gmail.com>
2016-05-22 11:16:27 +1000

    missed one whitespace

--- chatcommunicate.py
+++ chatcommunicate.py
@@ -302,7 +302,7 @@ def handle_commands(content_lower, message_parts, ev_room, ev_room_name, ev_user
 
             return "Recorded answer as an NAA in metasmoke."
 
-        if (second_part_lower.startswith("delete") or second_part_lower.startswith("remove") or second_part_lower.startswith("gone") or second_part_lower.startswith("poof") or second_part_lower == "del" ) and is_privileged(ev_room, ev_user_id, wrap2):
+        if (second_part_lower.startswith("delete") or second_part_lower.startswith("remove") or second_part_lower.startswith("gone") or second_part_lower.startswith("poof") or second_part_lower == "del") and is_privileged(ev_room, ev_user_id, wrap2):
             try:
                 msg.delete()
             except:
