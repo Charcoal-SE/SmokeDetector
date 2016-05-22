047f6e6a3fd1349bf0c6f799fd4cf1ebae4dded2
Quill <seriouslyquill@gmail.com>
2016-05-23 07:23:53 +1000

    Fixed all comments

--- metasmoke.py
+++ metasmoke.py
@@ -101,7 +101,7 @@ class Metasmoke:
                     autopull_message = "Message contains 'autopull', pulling..."
                     GlobalVars.charcoal_hq.send_message(
                         "[Continuous integration]({status}) on commit [{commit}](//github.com/Charcoal-SE/SmokeDetector/commit/{sha}) ".format(status=commit_response["ci_url"], commit=commit_response["commit_sha"][:7], sha=commit_response["commit_sha"]) +
-                        "(*{commit}*) succeeded!{optional_autopull_message}".format(commit=ommit_response["commit_message"].split("\n")[0], optional_autopull_message=autopull_message if "autopull" in commit_response["commit_message"] else "")
+                        "(*{commit}*) succeeded!{optional_autopull_message}".format(commit=commit_response["commit_message"].split("\n")[0], optional_autopull_message=autopull_message if "autopull" in commit_response["commit_message"] else "")
                     )
                     if "autopull" in commit_response["commit_message"]:
                         time.sleep(2)
