d907473ee7483ef0c5264635b344557868fcfb3b
Normal Human <normalhuman@users.noreply.github.com>
2016-06-02 17:06:57 +1000

    return if unable to parse JSON

--- spamhandling.py
+++ spamhandling.py
@@ -50,6 +50,7 @@ def check_if_spam_json(json_data):
         data = json.loads(json.loads(json_data)["data"])
     except ValueError:
         GlobalVars.charcoal_hq.send_message(u"Encountered ValueError parsing the following:\n{0}".format(json_data), False)
+        return False, None, ""
     if "ownerUrl" not in data:
         # owner's account doesn't exist anymore, no need to post it in chat:
         # http://chat.stackexchange.com/transcript/message/18380776#18380776
