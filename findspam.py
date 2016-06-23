b5c4b4e8f4a15c4197d4cd0a50ab004cf5a18521
Normal Human <normalhuman@users.noreply.github.com>
2016-06-23 20:53:15 +1000

    redbubble, joomag

--- findspam.py
+++ findspam.py
@@ -400,7 +400,7 @@ class FindSpam:
         "discussfact\\.com", "sirabhinavjain\\.com", "moneytransfercomparison\\.co",
         "exchangeratetoday\\.co", "currencysolutions\\.co", "accudb\\.com",
         "grey-areaweed\\.com", "360musicng\\.co", "oyetrade\\.com",
-        "trickspaid\\.com", "bulksmsclub\\.com",
+        "trickspaid\\.com", "bulksmsclub\\.com", "redbubble\\.com", "joomag\\.com",
     ]
     # Patterns: the top three lines are the most straightforward, matching any site with this string in domain name
     pattern_websites = [
