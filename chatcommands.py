94000391b34217fdb708e21d5e10d15ea66a2512
AWegnerGitHub <andy@andrewwegner.com>
2016-07-11 23:59:47 -0500

    Remove outdated TODO

--- chatcommands.py
+++ chatcommands.py
@@ -25,10 +25,6 @@ import datahandling
 def check_permissions(function):
     def run_command(ev_room, ev_user_id, wrap2, *args, **kwargs):
         if datahandling.is_privileged(ev_room, ev_user_id, wrap2):
-            # DOING THIS HAS AN ISSUE:
-            # The passed parameters - room_id and user_id are not passed to function().
-            # To get those, we need to either readd them to kwargs or change all
-            # function signatures to accept these as named parameters
             kwargs['ev_room'] = ev_room
             kwargs['ev_user_id'] = ev_user_id
             kwargs['wrap2'] = wrap2
