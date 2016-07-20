09be68fb7ed17621e489e1d69f25cd832fdc63e5
Normal Human <normalhuman@users.noreply.github.com>
2016-07-20 00:55:19 -0400

    quick fix to bool errorrs

--- chatcommunicate.py
+++ chatcommunicate.py
@@ -130,6 +130,8 @@ def watcher(ev, wrap2):
                                          wrap2=wrap2,
                                          content=current_message,
                                          message_id=message_id)
+                if not result:    # avoiding errors due to unprivileged commands 
+                    result = Response(command_status=True, message=None)
                 if result.command_status and result.message:
                     reply += result.message + os.linesep
                 if result.message is None:
