1f55fd9948b73c9957f730166aaa0121721c52a3
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2016-08-12 12:50:12 +0100

    +romrootandroid

--- findspam.py
+++ findspam.py
@@ -433,7 +433,7 @@ class FindSpam:
         "trickspaid\\.com", "bulksmsclub\\.com", "redbubble\\.com", "joomag\\.com",
         "ladybirdco\\.com", "magehit\\.com", "colors34.com", "ultimatewowguide\\.com", "fiverr\\.com/\\w+",
         "shaperich\\.com", "ugccoaching\\.com", "androidpluspc\\.com", "learnit\\.technology", "mastersite\\.com\\.ua",
-        "jobstelangana\\.in", "readyscript\\.ru", "unlock-bootloaderb\\.xyz"
+        "jobstelangana\\.in", "readyscript\\.ru", "unlock-bootloaderb\\.xyz", "romrootandroid\\.xyz",
     ]
     # Patterns: the top three lines are the most straightforward, matching any site with this string in domain name
     pattern_websites = [
