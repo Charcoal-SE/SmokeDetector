2778e37fce5fc4c698be7ecbb7fb8cabbc7e65a2
Normal Human <normalhuman@users.noreply.github.com>
2016-08-12 12:41:05 -0400

    test-a should test only as answer

--- chatcommands.py
+++ chatcommands.py
@@ -581,7 +581,7 @@ def command_test_answer(content, content_lower, *args, **kwargs):
     if len(string_to_test) == 0:
         return Response(command_status=True, message="Nothing to test")
     result = "> "
-    reasons, why = FindSpam.test_post(string_to_test, string_to_test, string_to_test, "", test_as_answer, False, 1, 0)
+    reasons, why = FindSpam.test_post("Valid title", string_to_test, "Valid username", "", test_as_answer, False, 1, 0)
     if len(reasons) == 0:
         result += "Would not be caught for title, answer, and username."
         return Response(command_status=True, message=result)
