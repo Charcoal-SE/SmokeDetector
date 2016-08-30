d5126d6d4e700d9feb68ae299cfda07b7028a3df
dorukayhan <dorukayhan@gmail.com>
2016-08-30 16:46:40 -0400

    Plop!
    Added a command that replies with "Plop!". The problem is, since I have no idea which function retrieves commands and executes things accordingly, I don't know where to call command_plop when someone says !!/plop...

--- chatcommands.py
+++ chatcommands.py
@@ -298,6 +298,13 @@ def command_wut(*args, **kwargs):
     :return: A string
     """
     return Response(command_status=True, message="Whaddya mean, 'wut'? Humans...")
+    
+def command_plop(*args, **kwargs):
+    """
+    Returns a string equal to "Plop!" (This is a joke command)
+    :return: A string
+    """
+    return Response(command_status=True, message="Plop!")
 
 
 # --- Block application from posting functions --- #
