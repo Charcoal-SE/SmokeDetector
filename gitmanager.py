fb7a1946b0f00cfba5cb588126f619d6a68943c6
angussidney <angussidney@users.noreply.github.com>
2016-10-11 20:26:42 +1100

    Standardize gitmanager so that it uses .format()

--- gitmanager.py
+++ gitmanager.py
@@ -31,14 +31,14 @@ class GitManager:
             blacklisted_websites.write("\n" + "\n".join(items_to_blacklist))
 
         # Checkout a new branch (mostly unnecessary, but may help if we create PRs in the future
-        branch = "auto-blacklist-%s" % str(time.time())
+        branch = "auto-blacklist-{0}".format(str(time.time()))
         git.checkout("-b", branch)
 
         # Clear HEAD just in case
         git.reset("HEAD")
 
         git.add("blacklisted_websites.txt")
-        git.commit("-m", "Auto blacklist of %s by %s --autopull" % (", ".join(items_to_blacklist), username))
+        git.commit("-m", "Auto blacklist of {0} by {1} --autopull".format(", ".join(items_to_blacklist), username)
 
         if code_permissions:
             git.checkout("master")
@@ -51,17 +51,17 @@ class GitManager:
             if GlobalVars.github_username is None or GlobalVars.github_password is None:
                 return (False, "tell someone to set a GH password")
 
-            payload = {"title": "%s: Blacklist %s" % (username, ", ".join(items_to_blacklist)),
-                       "body": "%s requests blacklist of domains: \n\n - %s" % (username, "\n - ".join(items_to_blacklist)),
+            payload = {"title": "{0}: Blacklist {1}".format(username, ", ".join(items_to_blacklist)),
+                       "body": "{0} requests blacklist of domains: \n\n - {1}".format(username, "\n - ".join(items_to_blacklist)),
                        "head": branch,
                        "base": "master"}
             response = requests.post("https://api.github.com/repos/Charcoal-SE/SmokeDetector/pulls", auth=HTTPBasicAuth(GlobalVars.github_username, GlobalVars.github_password), data=json.dumps(payload))
             print(response.json())
-            return (True, "You don't have code privileges, but I've [created a pull request for you](%s)." % response.json()["html_url"])
+            return (True, "You don't have code privileges, but I've [created a pull request for you]({0}).".format(response.json()["html_url"]))
 
         git.checkout(current_commit)  # Return to old commit to await CI. This will make Smokey think it's in reverted mode if it restarts
 
         if not code_permissions:
-            return (False, "Unable to perform action due to lack of code-level permissions. [Branch pushed](https://github.com/Charcoal-SE/SmokeDetector/tree/%s), PR at your leisure." % branch)
+            return (False, "Unable to perform action due to lack of code-level permissions. [Branch pushed](https://github.com/Charcoal-SE/SmokeDetector/tree/{0}), PR at your leisure.".format(branch))
 
         return (True, "Blacklisted {0} - the entry will be applied via autopull if CI succeeds.".format(", ".join(items_to_blacklist)))
