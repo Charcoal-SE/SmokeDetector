17f6aea5315e91e6a371b7006a39316bce77b0eb
dorukayhan <dorukayhan@gmail.com>
2016-10-25 17:10:34 -0400

    Blacklist 1-800-935-7817
    1-800-935-7817 seems to be the phone number of a new spammer trying to sell Viagra.

--- findspam.py
+++ findspam.py
@@ -275,7 +275,7 @@ class FindSpam:
         u"Ｃ[Ｏ|0]Ｍ", "ecoflex", "no2factor", "no2blast", "sunergetic", "capilux", "sante ?avis",
         "enduros", "dianabol", "ICQ#?\d{4}-?\d{5}", "3073598075", "lumieres", "viarex", "revimax",
         "celluria", "viatropin", "(meg|test)adrox", "nordic ?loan ?firm", "safflower",
-        "(essay|resume|article|dissertation|thesis) ?writing ?service", "satta ?matka", "bojiter"
+        "(essay|resume|article|dissertation|thesis) ?writing ?service", "satta ?matka", "bojiter", "1-?800-?935-?7817"
     ]
 
     blacklisted_websites = [line.rstrip() for line in open("blacklisted_websites.txt") if len(line.rstrip()) > 0]
