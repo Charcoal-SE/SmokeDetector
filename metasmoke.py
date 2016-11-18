288080b255416cb396dce61b8d8592478370c606
Undo1 <parker@erwaysoftware.com>
2016-11-18 13:12:36 -0700

    Temporarily remove websocket error handling

--- metasmoke.py
+++ metasmoke.py
@@ -17,77 +17,74 @@ import spamhandling
 class Metasmoke:
     @classmethod
     def init_websocket(self):
-        try:
-            GlobalVars.metasmoke_ws = websocket.create_connection(GlobalVars.metasmoke_ws_host, origin=GlobalVars.metasmoke_host)
-            GlobalVars.metasmoke_ws.send(json.dumps({"command": "subscribe", "identifier": "{\"channel\":\"SmokeDetectorChannel\",\"key\":\"" + GlobalVars.metasmoke_key + "\"}"}))
+        GlobalVars.metasmoke_ws = websocket.create_connection(GlobalVars.metasmoke_ws_host, origin=GlobalVars.metasmoke_host)
+        GlobalVars.metasmoke_ws.send(json.dumps({"command": "subscribe", "identifier": "{\"channel\":\"SmokeDetectorChannel\",\"key\":\"" + GlobalVars.metasmoke_key + "\"}"}))
 
-            while True:
-                a = GlobalVars.metasmoke_ws.recv()
-                print(a)
+        while True:
+            a = GlobalVars.metasmoke_ws.recv()
+            print(a)
+            try:
+                data = json.loads(a)
+                if "message" in data:
+                    message = data['message']
+                    if isinstance(message, Iterable):
+                        if "message" in message:
+                            GlobalVars.charcoal_hq.send_message(message['message'])
+                        elif "blacklist" in message:
+                            datahandling.add_blacklisted_user((message['blacklist']['uid'], message['blacklist']['site']), "metasmoke", message['blacklist']['post'])
+                        elif "naa" in message:
+                            post_site_id = parsing.fetch_post_id_and_site_from_url(message["naa"]["post_link"])
+                            datahandling.add_ignored_post(post_site_id[0:2])
+                        elif "fp" in message:
+                            post_site_id = parsing.fetch_post_id_and_site_from_url(message["fp"]["post_link"])
+                            datahandling.add_false_positive(post_site_id[0:2])
+                        elif "report" in message:
+                            post_data = apigetpost.api_get_post(message["report"]["post_link"])
+                            if post_data is None or post_data is False:
+                                continue
+                            if datahandling.has_already_been_posted(post_data.site, post_data.post_id, post_data.title) and not datahandling.is_false_positive((post_data.post_id, post_data.site)):
+                                continue
+                            user = parsing.get_user_from_url(post_data.owner_url)
+                            if user is not None:
+                                datahandling.add_blacklisted_user(user, "metasmoke", post_data.post_url)
+                            why = u"Post manually reported by user *{}* from metasmoke.\n".format(message["report"]["user"])
+                            spamhandling.handle_spam(title=post_data.title,
+                                                     body=post_data.body,
+                                                     poster=post_data.owner_name,
+                                                     site=post_data.site,
+                                                     post_url=post_data.post_url,
+                                                     poster_url=post_data.owner_url,
+                                                     post_id=post_data.post_id,
+                                                     reasons=["Manually reported " + post_data.post_type],
+                                                     is_answer=post_data.post_type == "answer",
+                                                     why=why,
+                                                     owner_rep=post_data.owner_rep,
+                                                     post_score=post_data.score,
+                                                     up_vote_count=post_data.up_vote_count,
+                                                     down_vote_count=post_data.down_vote_count,
+                                                     question_id=post_data.question_id)
+                        elif "commit_status" in message:
+                            c = message["commit_status"]
+                            sha = c["commit_sha"][:7]
+                            if c["commit_sha"] != os.popen('git log --pretty=format:"%H" -n 1').read():
+                                if c["status"] == "success":
+                                    if "autopull" in c["commit_message"]:
+                                        GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} succeeded. Message contains 'autopull', pulling...".format(ci_link=c["ci_url"], commit_sha=sha))
+                                        time.sleep(2)
+                                        os._exit(3)
+                                    else:
+                                        GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} succeeded.".format(ci_link=c["ci_url"], commit_sha=sha))
+                                elif c["status"] == "failure":
+                                    GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} failed.".format(ci_link=c["ci_url"], commit_sha=sha))
+            except Exception, e:
+                GlobalVars.metasmoke_ws = websocket.create_connection(GlobalVars.metasmoke_ws_host, origin=GlobalVars.metasmoke_host)
+                GlobalVars.metasmoke_ws.send(json.dumps({"command": "subscribe", "identifier": "{\"channel\":\"SmokeDetectorChannel\"}"}))
+                print e
                 try:
-                    data = json.loads(a)
-                    if "message" in data:
-                        message = data['message']
-                        if isinstance(message, Iterable):
-                            if "message" in message:
-                                GlobalVars.charcoal_hq.send_message(message['message'])
-                            elif "blacklist" in message:
-                                datahandling.add_blacklisted_user((message['blacklist']['uid'], message['blacklist']['site']), "metasmoke", message['blacklist']['post'])
-                            elif "naa" in message:
-                                post_site_id = parsing.fetch_post_id_and_site_from_url(message["naa"]["post_link"])
-                                datahandling.add_ignored_post(post_site_id[0:2])
-                            elif "fp" in message:
-                                post_site_id = parsing.fetch_post_id_and_site_from_url(message["fp"]["post_link"])
-                                datahandling.add_false_positive(post_site_id[0:2])
-                            elif "report" in message:
-                                post_data = apigetpost.api_get_post(message["report"]["post_link"])
-                                if post_data is None or post_data is False:
-                                    continue
-                                if datahandling.has_already_been_posted(post_data.site, post_data.post_id, post_data.title) and not datahandling.is_false_positive((post_data.post_id, post_data.site)):
-                                    continue
-                                user = parsing.get_user_from_url(post_data.owner_url)
-                                if user is not None:
-                                    datahandling.add_blacklisted_user(user, "metasmoke", post_data.post_url)
-                                why = u"Post manually reported by user *{}* from metasmoke.\n".format(message["report"]["user"])
-                                spamhandling.handle_spam(title=post_data.title,
-                                                         body=post_data.body,
-                                                         poster=post_data.owner_name,
-                                                         site=post_data.site,
-                                                         post_url=post_data.post_url,
-                                                         poster_url=post_data.owner_url,
-                                                         post_id=post_data.post_id,
-                                                         reasons=["Manually reported " + post_data.post_type],
-                                                         is_answer=post_data.post_type == "answer",
-                                                         why=why,
-                                                         owner_rep=post_data.owner_rep,
-                                                         post_score=post_data.score,
-                                                         up_vote_count=post_data.up_vote_count,
-                                                         down_vote_count=post_data.down_vote_count,
-                                                         question_id=post_data.question_id)
-                            elif "commit_status" in message:
-                                c = message["commit_status"]
-                                sha = c["commit_sha"][:7]
-                                if c["commit_sha"] != os.popen('git log --pretty=format:"%H" -n 1').read():
-                                    if c["status"] == "success":
-                                        if "autopull" in c["commit_message"]:
-                                            GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} succeeded. Message contains 'autopull', pulling...".format(ci_link=c["ci_url"], commit_sha=sha))
-                                            time.sleep(2)
-                                            os._exit(3)
-                                        else:
-                                            GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} succeeded.".format(ci_link=c["ci_url"], commit_sha=sha))
-                                    elif c["status"] == "failure":
-                                        GlobalVars.charcoal_hq.send_message("[CI]({ci_link}) on {commit_sha} failed.".format(ci_link=c["ci_url"], commit_sha=sha))
-                except Exception, e:
-                    GlobalVars.metasmoke_ws = websocket.create_connection(GlobalVars.metasmoke_ws_host, origin=GlobalVars.metasmoke_host)
-                    GlobalVars.metasmoke_ws.send(json.dumps({"command": "subscribe", "identifier": "{\"channel\":\"SmokeDetectorChannel\"}"}))
-                    print e
-                    try:
-                        exc_info = sys.exc_info()
-                        traceback.print_exception(*exc_info)
-                    except:
-                        print "meh"
-        except:
-            print "Couldn't bind to MS websocket"
+                    exc_info = sys.exc_info()
+                    traceback.print_exception(*exc_info)
+                except:
+                    print "meh"
 
     @classmethod
     def send_stats_on_post(self, title, link, reasons, body, username, user_link, why, owner_rep, post_score, up_vote_count, down_vote_count):
