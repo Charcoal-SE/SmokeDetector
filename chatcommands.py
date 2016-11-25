e6f29e21b588d8308fe3bc9edf81fb18f1ee0a67
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-11-25 15:38:45 +0000

    are you happy yet, flake

--- chatcommands.py
+++ chatcommands.py
@@ -253,10 +253,11 @@ def command_blacklist(message_parts, ev_user_name, ev_room, ev_user_id, wrap2, *
 
     chat_user_profile_link = "http://chat.{host}/users/{id}".format(host=wrap2.host, id=str(ev_user_id))
     result = GitManager.add_to_blacklist(
-            items_to_blacklist=message_parts[1:],
-            username=ev_user_name,
-            chat_profile_link=chat_user_profile_link,
-            code_permissions=datahandling.is_code_privileged(ev_room, ev_user_id, wrap2))
+        items_to_blacklist=message_parts[1:],
+        username=ev_user_name,
+        chat_profile_link=chat_user_profile_link,
+        code_permissions=datahandling.is_code_privileged(ev_room, ev_user_id, wrap2)
+    )
     return Response(command_status=result[0], message=result[1])
 
 
