faa20cffd96d383c11b00fea7aee985605c95f84
Thomas Ward <teward@users.noreply.github.com>
2017-03-03 15:35:26 -0500

    Use ChatLogon functions for chat logons instead.

--- ws.py
+++ ws.py
@@ -31,6 +31,7 @@ import json
 import time
 import requests
 from tld.utils import update_tld_names, TldIOError
+from chat_logon import ChatLogon
 
 try:
     update_tld_names()
@@ -73,46 +74,13 @@ load_files()
 filter_auto_ignored_posts()
 
 # chat.stackexchange.com logon/wrapper
-chatlogoncount = 0
-for cl in range(1, 10):
-    chatlogoncount += 1
-    try:
-        GlobalVars.wrap.login(username, password)
-        GlobalVars.smokeDetector_user_id[GlobalVars.charcoal_room_id] = str(GlobalVars.wrap.get_me().id)
-        break  # If we didn't error out horribly, we can be done with this loop
-    except (ValueError, AssertionError):
-        time.sleep(1)
-        continue  # If we did error, we need to try this again.
-if chatlogoncount >= 10:  # Handle "too many logon attempts" case to prevent infinite looping
-    raise RuntimeError("Could not get Chat.SE logon.")
+ChatLogon.SE(username, password)
 
 # chat.meta.stackexchange.com logon/wrapper
-metalogoncount = 0
-for cml in range(1, 10):
-    metalogoncount += 1
-    try:
-        GlobalVars.wrapm.login(username, password)
-        GlobalVars.smokeDetector_user_id[GlobalVars.meta_tavern_room_id] = str(GlobalVars.wrapm.get_me().id)
-        break  # If we didn't error out horribly, we can be done with this loop
-    except (ValueError, AssertionError):
-        time.sleep(1)
-        continue  # If we did error, we need to try this again.
-if metalogoncount >= 10:  # Handle "too many logon attempts" case to prevent infinite looping
-        raise RuntimeError("Could not get Chat.Meta.SE logon.")
+ChatLogon.MetaSE(username, password)
 
 # chat.stackoverflow.com logon/wrapper
-sologoncount = 0
-for sol in range(1, 10):
-    sologoncount += 1
-    try:
-        GlobalVars.wrapso.login(username, password)
-        GlobalVars.smokeDetector_user_id[GlobalVars.socvr_room_id] = str(GlobalVars.wrapso.get_me().id)
-        break  # If we didn't error out horribly, we can be done with this loop
-    except (ValueError, AssertionError):
-        time.sleep(1)
-        continue  # If we did error, we need to try this again.
-if sologoncount >= 10:  # Handle "too many logon attempts" case to prevent infinite looping
-    raise RuntimeError("Could not get Chat.SO logon.")
+ChatLogon.SO(username, password)
 
 GlobalVars.s = "[ " + GlobalVars.chatmessage_prefix + " ] " \
                "SmokeDetector started at [rev " +\
