4d2d7381f683990c5edb4b7bf3a3ba33c935c048
ArtOfCode <owen.jenkins2903@gmail.com>
2017-03-24 22:47:10 +0000

    Unicode and ASCII, this'll go well

--- helpers.py
+++ helpers.py
@@ -20,5 +20,5 @@ def all_matches_unique(match):
 
 
 def log(*args):
-    log_str = "[{}] {}".format(datetime.now().isoformat()[11:-7], "  ".join(args))
+    log_str = u"[{}] {}".format(datetime.now().isoformat()[11:-7], "  ".join(args))
     print(log_str)
