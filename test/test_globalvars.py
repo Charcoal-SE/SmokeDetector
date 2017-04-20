b6229f42994ea0b03b3eda9cf99f85a040a8cbfe
Thomas Ward <teward@ubuntu.com>
2017-04-20 17:46:55 -0400

    Random empty space to trigger new Circle builds

--- test/test_globalvars.py
+++ test/test_globalvars.py
@@ -19,6 +19,7 @@ def test_globalvars():
     assert GlobalVars.blockedTime[GlobalVars.charcoal_room_id] == 0
     assert GlobalVars.blockedTime[GlobalVars.meta_tavern_room_id] == 0
     assert GlobalVars.blockedTime[GlobalVars.socvr_room_id] == 0
+
     # The following lists must be empty in globalvars.py, because
     # they will be filled later.
     assert len(GlobalVars.auto_ignored_posts) == 0
