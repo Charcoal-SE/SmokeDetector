7d034faebf3ba14406d22c78ebe3425583cd70bc
Thomas Ward <teward@users.noreply.github.com>
2017-04-21 16:24:55 -0400

    Flake8 fixes

--- globalvars.py
+++ globalvars.py
@@ -322,8 +322,8 @@ class GlobalVars:
     commit = git_commit_info()
     if md5(commit['author'][0].encode('utf-8')).hexdigest() in censored_committer_names:
         commit['author'] = censored_committer_names[md5(commit['author'][0].encode('utf-8')).hexdigest()]
-    
-    commit_with_author = "%s (%s: *%s*)" % (commit['id'], 
+
+    commit_with_author = "%s (%s: *%s*)" % (commit['id'],
                                             commit['author'][0] if type(commit['author']) is list else commit['author'],
                                             commit['message'])
 
