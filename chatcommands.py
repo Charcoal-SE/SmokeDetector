818e279d815f9caa9b90ecd3c723841308a370ae
Jed Fox <git@twopointzero.us>
2017-05-08 13:47:32 -0400

    Apparently, timedelta only has `seconds` and `days`.

--- chatcommands.py
+++ chatcommands.py
@@ -3,8 +3,7 @@
 from globalvars import GlobalVars
 from findspam import FindSpam
 # noinspection PyUnresolvedReferences
-from datetime import datetime
-import datetime as dt
+from datetime import datetime, timedelta
 from utcdate import UtcDate
 from apigetpost import api_get_post
 from datahandling import *
@@ -445,7 +444,7 @@ def command_hats(*args, **kwargs):
         daystr = "days" if diff.days != 1 else "day"
         hourstr = "hours" if hours != 1 else "hour"
         minutestr = "minutes" if minutes != 1 else "minute"
-        secondstr = "seconds" if seconds != 1 else "second"
+        secondstr = "seconds" if seconds != 1 else "diff_second"
         return_string = "WE LOVE HATS! Winter Bash will begin in {} {}, {} {}, {} {}, and {} {}.".format(
             diff.days, daystr, hours, hourstr, minutes, minutestr, seconds, secondstr)
     elif wb_end > now:
@@ -726,23 +725,27 @@ def command_stappit(message_parts, ev_room, ev_user_id, wrap2, *args, **kwargs):
     return Response(command_status=True, message=None)
 
 def diff_time(diff):
-    if diff.minutes < 1:
-        time_str = '{seconds} second{plural}'.format(seconds=diff.seconds, plural='' if diff.seconds == 1 else 's')
-    elif diff.hours < 1:
-        time_str = '{minutes} minute{plural} '.format(minutes=diff.minutes, plural='' if diff.minutes == 1 else 's')
+    def pl(n, s='s'):
+        return '' if n == 1 else s
+    seconds = int(diff.total_seconds())
+    minutes = seconds // 60
+    hours = minutes // 60
+    days = hours // 24
+    if minutes < 1:
+        time_str = '{seconds} second{plural}'.format(seconds=seconds, plural=pl(seconds))
+    elif hours < 1:
+        time_str = '{minutes} minute{plural} '.format(minutes=minutes, plural=pl(minutes))
         diff = +diff
-        diff.minutes = 0
+        diff.seconds %= 60
         time_str += diff_time(diff)
-    elif diff.days < 1:
-        time_str = '{hours} hour{plural} '.format(hours=diff.hours, plural='' if diff.hours == 1 else 's')
+    elif days < 1:
+        time_str = '{hours} hour{plural} '.format(hours=hours, plural=pl(hours))
         diff = +diff
-        diff.hours = 0
+        diff.seconds %= 60 * 60
         time_str += diff_time(diff)
     else:
-        time_str = '{days} day{plural} '.format(days=diff.days, plural='' if diff.days == 1 else 's')
-        diff = +diff
-        diff.days = 0
-        time_str += diff_time(diff)
+        time_str = '{days} day{plural} '.format(days=days, plural=pl(days))
+        time_str += diff_time(timedelta(seconds=seconds % (60 * 60 * 24)))
     return time_str
 
 # noinspection PyIncorrectDocstring,PyUnusedLocal
