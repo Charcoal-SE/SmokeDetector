df7578992d9b6299160246acd497516ba3afaf07
angussidney <angussidney@users.noreply.github.com>
2017-07-22 12:27:41 +1000

    Try again

--- chatcommands.py
+++ chatcommands.py
@@ -504,8 +504,8 @@ def command_gitstatus(wrap2, *args, **kwargs):
 
 @check_permissions
 def command_remotediff(*args, **kwargs):
-    will_require_full_restart = "SmokeDetector will require a full restart to pull changes: {}".format(str(
-                                                                 only_blacklists_changed(GitManager.get_remote_diff())))
+    will_require_full_restart = "SmokeDetector will require a full restart to pull changes: " \
+                                "{}".format(str(only_blacklists_changed(GitManager.get_remote_diff())))
     return Response(command_status=True, message="\n\n".join([GitManager.get_remote_diff(), will_require_full_restart]))
 
 
