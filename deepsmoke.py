c2da48e522f03657db19ace5416d08c3ae9f222e
tripleee <tripleee@users.noreply.github.com>
2017-08-28 10:11:01 +0300

    check_deepsmoke: log timing
    To assess how much latency this check adds, log times.

--- deepsmoke.py
+++ deepsmoke.py
@@ -2,6 +2,7 @@
 # noinspection PyCompatibility
 
 import requests
+from datetime import datetime
 
 
 deepsmoke_url = 'http://99.239.154.69/dsd/index.php'
@@ -11,6 +12,7 @@ def check_deepsmoke(s, site, *args):
     """
     Query DeepSmoke for its verdict for this post.
     """
+    starttime = datetime.now()
     req = requests.get(deepsmoke_url, params={'q': s[0:270]})
     # Raise requests.exceptions.HTTPError for any server error
     req.raise_for_status()
@@ -19,7 +21,7 @@ def check_deepsmoke(s, site, *args):
     assert resp.endswith('</pre>\n')
     resp = resp[:-7]
     resp.rstrip('\n')
-    if resp == 'Spam':
-        return True, 'DeepSmoke response was {}'.format(resp)
-    # else
-    return False, ''
+    endtime = datetime.now()
+    details = 'DeepSmoke response was {0} (duration: {1})'.format(
+            resp, endtime - starttime)
+    return (resp=='Spam'), details
