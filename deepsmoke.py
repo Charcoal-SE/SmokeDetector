51b8976e8a3661a4e25016f32c68398fffedabb6
tripleee <tripleee@users.noreply.github.com>
2017-09-22 08:09:24 +0300

    deepsmoke.py: provisionally run JSON server on localhost
    The PHP server is gone and I have a Bottle replacement running locally now.

--- deepsmoke.py
+++ deepsmoke.py
@@ -5,7 +5,7 @@ import requests
 from datetime import datetime
 
 
-deepsmoke_url = 'http://99.239.154.69/dsd/index.php'
+deepsmoke_url = 'http://localhost:8080/spam'
 
 
 def check_deepsmoke(s, site, *args):
@@ -13,14 +13,14 @@ def check_deepsmoke(s, site, *args):
     Query DeepSmoke for its verdict for this post.
     """
     starttime = datetime.now()
-    req = requests.get(deepsmoke_url, params={'q': s[0:270]})
+    req = requests.get(deepsmoke_url, params={'body': s[0:270]})
     # Raise requests.exceptions.HTTPError for any server error
     req.raise_for_status()
-    assert req.text.startswith('<pre>')
-    resp = req.text[5:]
-    assert resp.endswith('</pre>\n')
-    resp = resp[:-7]
-    resp.rstrip('\n')
+    try:
+        result = req.json()
+        resp = result['spam']
+    except KeyError as err:
+        return False, 'error {0!r}'.format(err)
     endtime = datetime.now()
     details = 'DeepSmoke response was {0} (duration: {1})'.format(
         resp, endtime - starttime)
