2c369ad32c8f31c1bb348b934aac55e3f6b9a3e3
tripleee <tripleee@users.noreply.github.com>
2017-10-04 10:19:00 +0300

    Deepsmoke test case
    Somewhat oddly, Deepsmoke doesn't actually flag this post at the moment.

--- test/test_spamhandling.py
+++ test/test_spamhandling.py
@@ -51,6 +51,19 @@ with open("test/data_test_spamhandling.txt", "r", encoding="utf-8") as f:
     ('Pattern Matching product name - 2 words', """<p>vxl male enhancement</p>""", '', '', True),
     ('Pattern Matching product name - 3 words', """<p>Extends Monster Male Enhancement And Male Penile Enhancement</p>""", '', '', True),
     ('A Title', """<p>E x t e n d s  M o n s t e r Male E n h a n c e m e n t And M a l e P e n i l e E n h a n c e m e n t</p>""", '', 'judaism.stackexchange.com', True),
+    ('Deepsmoke test case', """<p>Use defines to stringify:</p>
+
+<pre><code>#define LENSTR_(x) #x
+#define LENSTR(x) LENSTR_(x)
+</code></pre>
+
+<p>then you can use:</p>
+
+<pre><code>#define MAXLEN 3
+
+char a[MAXLEN + 1];
+scanf(""%"" LENSTR(MAXLEN) ""s"", a);
+</code></pre>""", '', '', True),
 ])
 def test_check_if_spam(title, body, username, site, match):
     # We can't check blacklists/whitelists in tests, so these are set to their default values
