fbaf91d6fd90c33d0fa3853440cf253e9b2f136b
angussidney <4848802+angussidney@users.noreply.github.com>
2017-10-26 21:11:28 +1100

    Add alive, location and privileged commands

--- test/test_chatcommands.py
+++ test/test_chatcommands.py
@@ -1,8 +1,12 @@
 # noinspection PyUnresolvedReferences
 import chatcommunicate  # coverage
 import chatcommands
+from globalvars import GlobalVars
 import regex
 from unittest.mock import *
+
+
+# TODO: Test notifications, blacklisted and whitelisted users
 
 
 def test_coffee():
@@ -36,3 +40,28 @@ def test_brownie():
 
 def test_wut():
     assert chatcommands.wut() == "Whaddya mean, 'wut'? Humans..."
+
+
+def test_alive():
+    assert chatcommands.alive() in ['Yup', 'You doubt me?', 'Of course', '... did I miss something?',
+                                    'plz send teh coffee', 'Kinda sorta',
+                                    'Watching this endless list of new questions *never* gets boring']
+
+
+def test_location():
+    assert chatcommands.location() == GlobalVars.location
+
+
+def test_privileged():
+    chatcommunicate.parse_room_config("test/test_rooms.yml")
+
+    owner = Mock(name="El'endia Starman", id=1)
+    room = Mock(_client=Mock(host="chat.stackexchange.com"), id=11540)
+    msg = Mock(owner=owner, room=room)
+    assert chatcommands.amiprivileged(original_msg=msg) == "\u2713 You are a privileged user."
+
+    msg.owner.id = 2
+    assert chatcommands.amiprivileged(original_msg=msg) == "\u2573 " + GlobalVars.not_privileged_warning
+
+    msg.owner.is_moderator = True
+    assert chatcommands.amiprivileged(original_msg=msg) == "\u2713 You are a privileged user."
