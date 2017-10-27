97450f25110bdbbeea94c35031b200b092ec4d69
paper1111 <30367398+paper1111@users.noreply.github.com>
2017-10-27 20:54:00 +0800

    flake?

--- chatcommands.py
+++ chatcommands.py
@@ -1275,13 +1275,12 @@ def command_whois(message_parts, ev_user_id, wrap2, *args, **kwargs):
                                 wrap2.get_user(admin).last_message,
                                 wrap2.get_user(admin).last_seen)
                                for admin in admins_not_in_room]
-    
     singular_return_names = {"admin": "admin",
                              "code_admin": "code admin"}
 
     plural_return_names = {"admin": "admins",
                            "code_admin": "code admins"}
-    
+
     return_name = ""
     if len(admin_ids) == 1:
         return_name = singular_return_names[valid_roles[message_parts[1]]]
