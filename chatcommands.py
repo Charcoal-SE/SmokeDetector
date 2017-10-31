01124a0cc78078562c2d7535185f51d1195e7c64
paper1111 <30367398+paper1111@users.noreply.github.com>
2017-10-31 12:59:04 +0800

    Syntax

--- chatcommands.py
+++ chatcommands.py
@@ -1415,8 +1415,8 @@ def command_report_post(ev_room, ev_user_id, wrap2, message_parts, message_url,
                 se_link = parsing.to_protocol_relative(post_data.post_url)
                 ms_link = "https://m.erwaysoftware.com/posts/by-url?url={}".format(se_link)
                 output.append("Post {}: Already recently reported [[MS]({})]".format(index, ms_link))
-            else:
-                output.append("Post {}: Already recently reported".format(index)
+                continue
+            output.append("Post {}: Already recently reported".format(index)
             continue
         post_data.is_answer = (post_data.post_type == "answer")
         post = Post(api_response=post_data.as_dict)
