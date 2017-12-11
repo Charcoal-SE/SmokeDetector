88d5f60bdd92405a3982f7518654262c3bffdee2
ArtOfCode- <hello@artofcode.co.uk>
2017-12-11 18:45:46 +0000

    Maybe try this

--- blacklists.py
+++ blacklists.py
@@ -45,7 +45,8 @@ class BasicListParser(BlacklistParser):
         with open(self._filename, 'r+', encoding='utf-8') as f:
             items = f.readlines()
             items = [x for x in items if item not in x]
-            f.truncate(0)
+            f.seek(0)
+            f.truncate()
             f.writelines(items)
 
     def exists(self, item: str):
@@ -89,7 +90,8 @@ class TSVDictParser(BlacklistParser):
         with open(self._filename, 'r+', encoding='utf-8') as f:
             items = f.readlines()
             items = [x for x in items if item not in x]
-            f.truncate(0)
+            f.seek(0)
+            f.truncate()
             f.writelines(items)
 
     def exists(self, item: Union[str, dict]):
