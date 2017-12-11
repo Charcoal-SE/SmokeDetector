db70e148846915358512bfcd391bddb2ada4b6ba
ArtOfCode- <hello@artofcode.co.uk>
2017-12-11 13:06:05 +0000

    Move Feedback into classes/

--- chatcommands.py
+++ chatcommands.py
@@ -26,6 +26,7 @@ from html import unescape
 import regex
 from helpers import only_blacklists_changed
 from classes import Post
+from classes.feedback import *
 
 
 # TODO: Do we need uid == -2 check?  Turn into "is_user_valid" check
@@ -1231,60 +1232,6 @@ def postgone(msg):
         raise CmdException("That's not a report.")
 
     msg.edit(edited)
-
-
-class Feedback:
-    TRUE_POSITIVE = "tp"
-    FALSE_POSITIVE = "fp"
-    NAA = "naa"
-
-    def __init__(self, feedback, blacklist=False, always_silent=False):
-        self.blacklist = blacklist
-        self.always_silent = always_silent
-
-        self._type = feedback + ("u" if blacklist else "") + ("-" if always_silent else "")
-
-    def send(self, url, msg):
-        Feedback.send_custom(self._type, url, msg)
-
-    @staticmethod
-    def send_custom(type, url, msg):
-        threading.Thread(name="metasmoke feedback send on " + url,
-                         target=Metasmoke.send_feedback_for_post,
-                         args=(url, type, msg.owner.name, msg.owner.id, msg._client.host,)).start()
-
-
-TRUE_FEEDBACKS = {
-    "true": Feedback(Feedback.TRUE_POSITIVE, always_silent=False, blacklist=False),
-    "tp": Feedback(Feedback.TRUE_POSITIVE, blacklist=False, always_silent=False),
-    "trueu": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=False),
-    "tpu": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=False),
-
-    "k": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=True),
-    "spam": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=True),
-    "rude": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=True),
-    "abusive": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=True),
-    "offensive": Feedback(Feedback.TRUE_POSITIVE, blacklist=True, always_silent=True),
-
-    "v": Feedback(Feedback.TRUE_POSITIVE, blacklist=False, always_silent=True),
-    "vand": Feedback(Feedback.TRUE_POSITIVE, blacklist=False, always_silent=True),
-    "vandalism": Feedback(Feedback.TRUE_POSITIVE, blacklist=False, always_silent=False)
-}
-
-FALSE_FEEDBACKS = {
-    "false": Feedback(Feedback.FALSE_POSITIVE, blacklist=False, always_silent=False),
-    "fp": Feedback(Feedback.FALSE_POSITIVE, blacklist=False, always_silent=False),
-    "falseu": Feedback(Feedback.FALSE_POSITIVE, blacklist=True, always_silent=False),
-    "fpu": Feedback(Feedback.FALSE_POSITIVE, blacklist=True, always_silent=False),
-
-    "f": Feedback(Feedback.FALSE_POSITIVE, blacklist=False, always_silent=True),
-    "notspam": Feedback(Feedback.FALSE_POSITIVE, blacklist=False, always_silent=True),
-}
-
-NAA_FEEDBACKS = {
-    "naa": Feedback(Feedback.NAA, blacklist=False, always_silent=False),
-    "n": Feedback(Feedback.NAA, blacklist=False, always_silent=True),
-}
 
 
 # noinspection PyIncorrectDocstring
