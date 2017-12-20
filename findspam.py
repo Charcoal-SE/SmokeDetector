385e93ef8e9caadd60c46b4fe05c5cd1079b476a
Undo1 <parker@erwaysoftware.com>
2017-12-19 18:22:37 -0700

    Restrict title-ending-in-comma to IPS

--- findspam.py
+++ findspam.py
@@ -1142,7 +1142,10 @@ class FindSpam:
         {'method': mostly_dots, 'all': True, 'sites': ['codegolf.stackexchange.com'],
          'reason': 'mostly dots in {}', 'title': True, 'body': True, 'username': False, 'body_summary': False,
          'stripcodeblocks': False, 'max_rep': 50, 'max_score': 0},
-
+        # Title ends with Comma (IPS Troll)
+        {'regex': r".*\,$", 'all': False, 'sites': ['interpersonal.stackexchange.com'],
+         'reason': "title ends with comma", 'title': True, 'body': False, 'username': False, 'stripcodeblocks': False,
+         'body_summary': False, 'max_rep': 50, 'max_score': 0},
         #
         # Category: other
         # Blacklisted usernames
