2dc7c9d417cadad7fa3a70f6bf0319dd96e2baf5
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2017-12-29 23:32:21 +0000

    Revert clobberererererer

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -6,9 +6,7 @@ import helpers
     ('https://goo.gl/kAb9rz', 'https://charcoal-se.org/'),
     ('https://bit.ly/2jhMbxn', 'https://charcoal-se.org/'),
     ('https://tinyurl.com/y7ba4pth', 'https://charcoal-se.org/'),
-    ('https://tiny.cc/gsbbpy', 'https://charcoal-se.org/'),
-    ('http://ea84eef0.ngrok.io/api/v2.0/posts/search.atom?key=abcdef012345&site=stackoverflow.com',
-     'http://ea84eef0.ngrok.io/api/v2.0/posts/search.atom?key=abcdef012345&site=stackoverflow.com')
+    ('https://tiny.cc/gsbbpy', 'https://charcoal-se.org/')
 ])
 def test_unshorten_link(shortened, original):
     assert helpers.unshorten_link(shortened) == original
