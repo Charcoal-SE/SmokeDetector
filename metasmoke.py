eb3dccc97d0c0d1a6a8bf2f3b079d3652d398e27
quartata <quartata@users.noreply.github.com>
2018-02-09 13:53:15 -0800

    flake

--- metasmoke.py
+++ metasmoke.py
@@ -285,7 +285,7 @@ class Metasmoke:
                         GlobalVars.standby_mode = False
                         GlobalVars.metasmoke_last_ping_time = datetime.now()  # Otherwise the ping watcher will exit(10)
 
-                        chatcommunicate.tell_rooms_with("debug", GlobalVars.location + " received failover signal.", 
+                        chatcommunicate.tell_rooms_with("debug", GlobalVars.location + " received failover signal.",
                                                         notify_site="/failover")
 
                     if response['standby']:
