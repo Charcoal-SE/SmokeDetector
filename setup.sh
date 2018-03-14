1586fbd5ef36c63d4192194239f107b43e92511b
iBug ♦ <7273074+iBug@users.noreply.github.com>
2018-03-14 12:22:43 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,6 @@
 
 git clone https://github.com/Charcoal-SE/SmokeDetector.git
 cd SmokeDetector
-sudo pip install -r requirements.txt --upgrade
+
+sudo -H python3 -m pip install -r requirements.txt --upgrade
+python3 -m pip install -r user_requirements.txt --upgrade
