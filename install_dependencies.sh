aec47b6d95a50e55917a1bf8e49fcde18cdd15ff
iBug ♦ <7273074+iBug@users.noreply.github.com>
2018-03-14 12:21:25 +0800

    Delete install_dependencies.sh

--- install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install system-wide Python library dependencies
-sudo -H python -m pip install -r requirements.txt --upgrade
-
-# Some dependencies need to be installed in user space.
-python -m pip install --user -r user_requirements.txt --upgrade
