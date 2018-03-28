7383694a8615ef35dbfe435d384d1417ecb0c676
angussidney <4848802+angussidney@users.noreply.github.com>
2018-03-28 19:22:30 +1100

    Re-add hats

--- chatcommands.py
+++ chatcommands.py
@@ -430,7 +430,6 @@ def wut():
     return "Whaddya mean, 'wut'? Humans..."
 
 
-"""
 @command(aliases=["zomg_hats"])
 def hats():
     wb_start = datetime(2017, 12, 13, 0, 0, 0)
@@ -459,7 +458,6 @@ def hats():
             diff.days, daystr, hours, hourstr, minutes, minutestr, seconds, secondstr)
 
     return return_string
-"""
 
 
 # --- Block application from posting functions --- #
