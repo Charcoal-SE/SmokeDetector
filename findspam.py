dadd7b976b896a6e82aad8594dbe57219fba6a5b
iBug <stevepad@me.com>
2018-05-03 12:00:24 +0800

    Replace title with placeholder to avoid checking

--- findspam.py
+++ findspam.py
@@ -1368,11 +1368,12 @@ class FindSpam:
         result = []
         why = {'title': [], 'body': [], 'username': []}
         for rule in FindSpam.rules:
-            body_to_check = post.body.replace("&nbsp;", "")
+            title_to_check = post.title if not post.is_answer else "Placeholder Title"
+            body_to_check = post.body.replace("&nbsp;", "").replace("\xAD", "") \
+                                     .replace("\u200B", "").replace("\u200C", "")
             is_regex_check = 'regex' in rule
             check_if_answer = rule.get('answers', True)
             check_if_question = rule.get('questions', True)
-            body_to_check = regex.sub("[\xad\u200b\u200c]", "", body_to_check)
             if rule['stripcodeblocks']:
                 # use a placeholder to avoid triggering "few unique characters" when most of post is code
                 body_to_check = regex.sub("(?s)<pre>.*?</pre>",
@@ -1391,7 +1392,7 @@ class FindSpam:
                 if is_regex_check:
                     compiled_regex = regex.compile(rule['regex'], regex.UNICODE, city=FindSpam.city_list)
                     # using a named list \L in some regexes
-                    matched_title = compiled_regex.findall(post.title)
+                    matched_title = compiled_regex.findall(title_to_check)
                     matched_username = compiled_regex.findall(post.user_name)
                     if (not post.body_is_summary or rule['body_summary']) and \
                             (not post.is_answer or check_if_answer) and \
@@ -1413,7 +1414,7 @@ class FindSpam:
                             why["body"].append(u"Post - {}".format(why_post))
                             result.append(rule['reason'].replace("{}", "answer" if post.is_answer else "body"))
                     else:
-                        matched_title, why_title = rule['method'](post.title, post.post_site, post.user_name)
+                        matched_title, why_title = rule['method'](title_to_check, post.post_site, post.user_name)
                         if matched_title and rule['title']:
                             why["title"].append(u"Title - {}".format(why_title))
                         matched_username, why_username = rule['method'](post.user_name, post.post_site, post.user_name)
@@ -1426,7 +1427,7 @@ class FindSpam:
                             if matched_body and rule['body']:
                                 why["body"].append(u"Post - {}".format(why_body))
                 if matched_title and rule['title']:
-                    why["title"].append(FindSpam.generate_why(compiled_regex, post.title, u"Title", is_regex_check))
+                    why["title"].append(FindSpam.generate_why(compiled_regex, title_to_check, u"Title", is_regex_check))
                     result.append(rule['reason'].replace("{}", "title"))
                 if matched_username and rule['username']:
                     why["username"].append(FindSpam.generate_why(compiled_regex, post.user_name, u"Username",
