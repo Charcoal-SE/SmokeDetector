4e9b01219f9fbdb04dcd22183bfd22d339032bb4
Welz <35234925+WELZ-gh@users.noreply.github.com>
2018-06-06 09:52:21 -0400

    added 'errlog' and 'errlogs' aliases.

--- chatcommands.py
+++ chatcommands.py
@@ -520,7 +520,7 @@ def alive():
 
 
 # noinspection PyIncorrectDocstring
-@command(int, privileged=True, arity=(0, 1))
+@command(int, privileged=True, arity=(0, 1), aliases=["errlogs", "errlog"])
 def errorlogs(count):
     """
     Shows the most recent lines in the error logs
