43d4001af5862578779006ff9462b5b01a3c95fa
tripleee <tripleee@users.noreply.github.com>
2018-06-08 13:33:15 +0300

    Add Nashik and Gurgaon to Indian places --autopull

--- findspam.py
+++ findspam.py
@@ -995,6 +995,7 @@ class FindSpam:
         "Ludhiana", "Mumbai", "Madurai", "Patna", "Portland",
         "Rajkot", "Surat", "Telangana", "Udaipur", "Uttarakhand",
         "Noida", "Pune", "Rohini", "Trivandrum", "Thiruvananthapuram",
+        "Nashik", "Gurgaon",
         # yes, these aren't cities but...
         "India", "Pakistan",
         # buyabans.com spammer uses creative variations
