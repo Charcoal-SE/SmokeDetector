d646c73f5b949934b89aa243f9b745c68aae3f80
iBug <stevepad@me.com>
2018-06-14 19:54:35 +0800

    Use `python3` instead of `python`

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install system-wide Python library dependencies
-sudo -H python -m pip install -r requirements.txt --upgrade
+sudo -H python3 -m pip install -r requirements.txt --upgrade
 
 # Some dependencies need to be installed in user space.
-python -m pip install --user -r user_requirements.txt --upgrade
+python3 -m pip install --user -r user_requirements.txt --upgrade
