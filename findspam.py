a55077cb3d8ed103200d25e7b16b1b9c949c8d13
iBug <stevepad@me.com>
2018-07-03 19:26:45 +0800

    `coupon` in link text --autopull

--- findspam.py
+++ findspam.py
@@ -420,7 +420,7 @@ def bad_link_text(s, site, *args):   # suspicious text of a hyperlink
         r"(?isu)"
         r"\b(buy|cheap) |live[ -]?stream|"
         r"\bmake (money|\$)|"
-        r"\b(porno?|(whole)?sale|coins|replica|luxury|essays?|in \L<city>)\b"
+        r"\b(porno?|(whole)?sale|coins|replica|luxury|coupons?|essays?|in \L<city>)\b"
         r"\b\L<city>(?:\b.{1.20}\b)?(service|escort|call girls?)|"
         r"(best|make|full|hd|software|cell|data)[\w ]{1,20}(online|service|company|repair|recovery|school|university)|"
         r"\b(writing (service|help)|essay (writing|tips))", city=FindSpam.city_list)
