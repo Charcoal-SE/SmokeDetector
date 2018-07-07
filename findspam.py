a812f68816c1bc96b529393d9563d847de96c012
iBug <stevepad@me.com>
2018-07-07 12:34:01 +0800

    Catch @>> as link following arrow --autopull

--- findspam.py
+++ findspam.py
@@ -1226,8 +1226,8 @@ class FindSpam:
          'reason': "pattern-matching website in {}", 'title': True, 'body': True, 'username': True,
          'stripcodeblocks': False, 'body_summary': True, 'max_rep': 4, 'max_score': 2},
         # Links preceded by arrows >>>
-        {'regex': r"(?is)(>>>+|==\s*>>+|====|===>+|==>>+|= = =|(Read More|Click Here)).{0,20}"
-                  r"http(?!://i.stack.imgur.com).{0,200}$", 'all': True,
+        {'regex': r"(?is)(?:>>>+|@+>>+|@@+>+|==\s*>>+|={4,}|===>+|= = =|(Read More|Click Here)).{0,20}"
+                  r"https?(?!://i.stack.imgur.com).{0,200}$", 'all': True,
          'sites': [], 'reason': "link following arrow in {}", 'title': True, 'body': True, 'username': True,
          'stripcodeblocks': True, 'body_summary': False, 'answers': False, 'max_rep': 11, 'max_score': 0},
         # Link at the end of question, selected sites
