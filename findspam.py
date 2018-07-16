c06b2ad87cd990848cc7933045ff5cfc20272337
iBug <stevepad@me.com>
2018-07-16 13:51:08 +0800

    Fix bad negative lookahead

--- findspam.py
+++ findspam.py
@@ -1265,7 +1265,7 @@ class FindSpam:
          'stripcodeblocks': False, 'body_summary': True, 'max_rep': 4, 'max_score': 2},
         # Links preceded by arrows >>>
         {'regex': r"(?is)(?:>>>+|@+>>+|@@+>+|==\s*>>+|={4,}|===>+|= = =|(Read More|Click Here)).{0,20}"
-                  r"https?(?!://i.stack.imgur.com)(?=.{,200}$)", 'all': True,
+                  r"https?://(?!i\.stack\.imgur\.com)(?=.{,200}$)", 'all': True,
          'sites': [], 'reason': "link following arrow in {}", 'title': True, 'body': True, 'username': True,
          'stripcodeblocks': True, 'body_summary': False, 'answers': False, 'max_rep': 11, 'max_score': 0},
         # Link at the end of question, selected sites
