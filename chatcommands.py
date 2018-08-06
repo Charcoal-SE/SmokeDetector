614328f36c66d6bf87bad1c526f78a04eadfc4c2
tripleee <tripleee@users.noreply.github.com>
2018-08-06 07:19:52 +0300

    check_blacklist(): lowercase "bad NS", duh
    Thanks to iBug for the review.

--- chatcommands.py
+++ chatcommands.py
@@ -207,7 +207,7 @@ def check_blacklist(string_to_test, is_username, is_watchlist, is_phone):
     reasons = list(set(question_reasons) | set(answer_reasons))
 
     # Filter out watchlist results
-    filter_out = ["potentially bad NS"]
+    filter_out = ["potentially bad ns"]
     if not is_watchlist:
         filter_out.append("potentially bad keyword")
     # Ignore "Mostly non-latin body/answer" for phone number watches
