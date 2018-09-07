0c16029ca047b642e5e8429520f2f596beb2a704
iBug <stevepad@me.com>
2018-09-07 18:48:58 +0800

    ci

--- findspam.py
+++ findspam.py
@@ -234,7 +234,7 @@ def has_repeating_characters(s, site):
     if not s:
         return False, ""
     # matches = regex.compile(r"([^\s_.,?!=~*/0-9-])(\1{9,})", regex.UNICODE).findall(s)
-    matches = regex.compile(r"([^\s_.])(\1{9,})", regex.UNICODE).findall(s)
+    matches = regex.compile(r"([^\s\d_.])(\1{9,})", regex.UNICODE).findall(s)
     match = "".join(["".join(match) for match in matches])
     if len(match) / len(s) >= REPEATED_CHARACTER_RATIO:  # Repeating characters make up >= 20 percent
         return True, "Repeated character: {}".format(", ".join(
