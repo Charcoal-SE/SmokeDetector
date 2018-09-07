bd3442c9a9a58373858b6b117a16aec9d1817497
iBug <stevepad@me.com>
2018-09-07 14:35:47 +0800

    shebang for executable scripts

--- nocrash.py
+++ nocrash.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-# coding=utf-8
+#!/usr/bin/env python3
 
 # This script replaces the original nocrash.sh functionality with a pure Python approach.
 
--- ws.py
+++ ws.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+#!/usr/bin/env python3
 # requires https://pypi.python.org/pypi/websocket-client/
 
 from excepthook import uncaught_exception, install_thread_excepthook
