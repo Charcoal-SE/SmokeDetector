dff1f9176d7ce77a242263bfc9a0760cd31f0585
iBug <stevepad@me.com>
2018-09-19 10:01:49 +0800

    Add a prototype for cached regex.compile()

---
+++ regex_proxy.py
@@ -0,0 +1,16 @@
+from regex import *
+from regex import compile as raw_compile
+
+
+_cache = {}
+
+
+# Wrap regex.compile up so we have a global cache
+def compile(s, *args, **args):
+    global _cache
+    try:
+        return _cache[s]
+    except KeyError:
+        r = raw_compile(s, *args, **kwargs)
+        _cache[s] = r
+        return r
