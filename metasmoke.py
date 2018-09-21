198f0e425fc4527d78ed9ac6e86442bb198694bf
ArtOfCode <ArtOfCode-@users.noreply.github.com>
2018-09-21 18:25:13 +0100

    I love you too, Flake --autopull

--- metasmoke.py
+++ metasmoke.py
@@ -78,7 +78,7 @@ class Metasmoke:
                            "to try and reset the connection states.", f=True)
             os._exit(10)
         elif GlobalVars.metasmoke_last_ping_time < (datetime.now() - timedelta(seconds=120)) \
-             and not GlobalVars.metasmoke_down:
+            and not GlobalVars.metasmoke_down:
             log('warning', "Last metasmoke ping with a response was over 120 seconds ago, "
                            "forcing SmokeDetector restart to reset all sockets.", f=True)
             os._exit(10)
@@ -324,7 +324,7 @@ class Metasmoke:
                 if 'failover' in response and GlobalVars.standby_mode:
                     if response['failover']:
                         GlobalVars.standby_mode = False
-                        
+
                         chatcommunicate.tell_rooms_with("debug", GlobalVars.location + " received failover signal.",
                                                         notify_site="/failover")
 
