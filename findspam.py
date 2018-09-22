220d9b439afcb94b089f75aff7f8793166cf1b88
iBug <stevepad@me.com>
2018-09-22 13:39:16 +0800

    autopull

--- findspam.py
+++ findspam.py
@@ -998,7 +998,8 @@ class FindSpam:
         " %[au]h ", "tapsi ?sarkar",
         "(?:networking|cisco|sas|hadoop|mapreduce|oracle|dba|php|sql|javascript|js|java|designing|marketing|"
         "salesforce|joomla)( certification)? (courses?|training)(?=.{,25}</a>)",
-        r"(?:design|development|compan(y|ies)|training|courses?|automation)(\b.{1,8}\b)?\L<city>\b",
+        r"(?:design|development|compan(y|ies)|training|courses?|automation|policy)"
+        r"(\b.{1,8}\b)?\L<city>\b",
         r"\b\L<city>(\b.{1,8}\b)?(?:tour)",  # TODO: Populate this "after city" keyword list
         u"Ｃ[Ｏ0]Ｍ", "sunergetic", "capilux",
         r"ICQ#?\d{4}-?\d{5}", "viarex",
