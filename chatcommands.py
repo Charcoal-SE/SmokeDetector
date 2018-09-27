b5600c86c4236cd27ceae5d9cb0a3f57d19b4aed
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2018-09-27 16:35:40 +0800

    Clearer message, autopull

--- chatcommands.py
+++ chatcommands.py
@@ -252,10 +252,10 @@ def do_blacklist(blacklist_type, msg, force=False):
     pattern = rebuild_str(msg.content_source.split(" ", 1)[1])
     try:
         r = regex.compile(pattern, city=findspam.FindSpam.city_list)
-        if r.search(GlobalVars.valid_content):
-            raise ValueError
-    except (regex._regex_core.error, ValueError):
+    except regex._regex_core.error:
         raise CmdException("An invalid pattern was provided, please check your command.")
+    if r.search(GlobalVars.valid_content):
+        raise CmdException("That pattern is probably too broad, refusing to commit.")
 
     if not force:
         if regex.match(r'(?:\[a-z_]\*)?(?:\(\?:)?\d+(?:[][\\W_*()?:]+\d+)+(?:\[a-z_]\*)?$', pattern):
