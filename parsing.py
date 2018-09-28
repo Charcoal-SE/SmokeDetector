7408717d2de4870e5547a7aa4c8cd3397665e2f2
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2018-09-28 11:06:42 +0800

    pytest HTTPS

--- parsing.py
+++ parsing.py
@@ -235,7 +235,7 @@ def user_url_to_shortlink(url):
     user_id_and_site = get_user_from_url(url)
     if user_id_and_site is None:
         return url
-    return "http://{}/users/{}".format(user_id_and_site[1], user_id_and_site[0])
+    return "https://{}/users/{}".format(user_id_and_site[1], user_id_and_site[0])
 
 
 # noinspection PyMissingTypeHints
