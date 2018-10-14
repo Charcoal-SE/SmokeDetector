77f8073bc72a6b119086ed20e0258dcd9569b322
tripleee <tripleee@users.noreply.github.com>
2018-10-14 15:35:02 +0300

    Watch AS 24778 --autopull
    DATAPIPE-UK, GB

--- findspam.py
+++ findspam.py
@@ -727,6 +727,7 @@ def watched_asn_for_url_hostname(s, site):
         [
             '3842',    # RAMNODE - RamNode LLC, US
             '8100',    # ASN-QUADRANET-GLOBAL - QuadraNet Enterprises LLC, US
+            '24778',   # DATAPIPE-UK, GB
             '34788',   # NMM-AS D - 02742 Friedersdorf Hauptstrasse 68, DE
             '40676',   # AS40676 - Psychz Networks, US
             '45839',   # SHINJIRU-MY-AS-AP Shinjiru Technology Sdn Bhd, MY
