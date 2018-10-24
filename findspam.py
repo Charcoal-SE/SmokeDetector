d2cffd23cd84f109d8ddaf59128f48ff0d4f8556
tripleee <tripleee@users.noreply.github.com>
2018-10-24 09:55:55 +0300

    Watch ASN 62731 247RACK-COM - 247RACK.com, US --autopull

--- findspam.py
+++ findspam.py
@@ -744,6 +744,7 @@ def watched_asn_for_url_hostname(s, site):
             '45839',   # SHINJIRU-MY-AS-AP Shinjiru Technology Sdn Bhd, MY
             '46261',   # QUICKPACKET - QuickPacket, LLC, US
             '54290',   # HOSTWINDS - Hostwinds LLC., US
+            '62731',   # 247RACK-COM - 247RACK.com, US
             '395970',  # IONSWITCH - IonSwitch, LLC, US
         ])
 
