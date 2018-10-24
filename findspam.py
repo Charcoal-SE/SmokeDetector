45ad5470a144049d777e4f3ccaf2cc3fd4625dcb
tripleee <tripleee@users.noreply.github.com>
2018-10-24 11:30:13 +0300

    Watch ASN 29073 QUASINETWORKS, NL --autopull

--- findspam.py
+++ findspam.py
@@ -736,6 +736,7 @@ def watched_asn_for_url_hostname(s, site):
             # '22612', # Moderate FPs, don't double up -- NAMECHEAP-NET - Namecheap, Inc., US
             # '26496', # Massive FPs do not watch -- AS-26496-GO-DADDY-COM-LLC - GoDaddy.com, LLC, US
             '24778',   # DATAPIPE-UK, GB
+            '29073',   # QUASINETWORKS, NL
             '29405',   # VNET-AS, SK
             '31863',   # DACEN-2 - Centrilogic, Inc., US
             '36352',   # AS-COLOCROSSING - ColoCrossing, US
