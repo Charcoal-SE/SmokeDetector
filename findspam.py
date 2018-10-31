8f86b5db33b017f7879d6f0482a2d279bc4dfed8
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2018-10-31 16:45:46 +0800

    ! Reduce > count to 2 for "link following arrow"

--- findspam.py
+++ findspam.py
@@ -1604,7 +1604,7 @@ class FindSpam:
          'reason': "pattern-matching website in {}", 'title': True, 'body': True, 'username': True,
          'stripcodeblocks': False, 'body_summary': True, 'max_rep': 4, 'max_score': 2},
         # Links preceded by arrows >>>
-        {'regex': r"(?is)(?:>>>+|@+>>+|@@+>+|==\s*>>+|={4,}|===>+|= = =|Read More|Click Here).{,20}"
+        {'regex': r"(?is)(?:>>+|@+>+|==\s*>+|={4,}|===>+|= = =|Read More|Click Here).{,20}"
                   r"https?://(?!i\.stack\.imgur\.com)(?=.{,200}$)", 'all': True,
          'sites': [], 'reason': "link following arrow in {}", 'title': True, 'body': True, 'username': True,
          'stripcodeblocks': True, 'body_summary': False, 'answers': False, 'max_rep': 11, 'max_score': 0},
