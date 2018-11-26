a37bda86bea0e441b967e50541e55098751baa13
tripleee <tripleee@users.noreply.github.com>
2018-11-26 10:11:59 +0200

    ! Watch IP 198.54.116.51

--- findspam.py
+++ findspam.py
@@ -1092,6 +1092,7 @@ def watched_ip_for_url_hostname(s, site):
             '104.219.248.81',   # drugs supplement4world / supplementgod etc
             '162.213.255.36',   # drugs / visit-my-website / cryptocurrency
             '198.54.115.65',    # drugs goldencondor / visit4supplements etc
+            '198.54.116.51',    # drugs reviewsgear / crazy-bulk-review etc
             '198.54.116.85',    # drugs mummydiet.org
             '198.54.120.134',   # drugs advisorwellness / health4supplement etc
             '198.54.126.109',   # escorts + buy-likes
