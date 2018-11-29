2d5ad79e79a7a4d7d37c71869829416ea364d95b
ArtOfCode- <hello@artofcode.co.uk>
2018-11-29 08:26:13 +0000

    I can't spell

--- test/test_socketscience.py
+++ test/test_socketscience.py
@@ -2,7 +2,7 @@ from socketscience import SocketScience
 import pytest
 
 
-@pytest.mark.parameterize('content', [
+@pytest.mark.parametrize('content', [
     (".\n\u00022046b'\x82\xa8location\xabHenders/EC2\xa9timestamp\xcbA\xd6\xff\xb6~\xaay\xc3'\u0003",),
     (".\n\u00023786b'\x82\xa8location\xabHenders/EC2\xa9timestamp\xcbA\xd6\xff\xb5\x8e\x9c \x93'\u0003",)
 ])
