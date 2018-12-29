4154ca07a3d878c0c36aee0d9f8a8043aebfdb60
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2018-12-29 21:47:15 +0800

    some more updates to util.py

--- util.py
+++ util.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import shlex
+import inspect
 
 import helpers
 helpers.log = lambda *args, **kwargs: None  # Override log() for less verbosity
@@ -18,6 +19,7 @@ def utility(name, description):
     def wrapper(func):
         global utilities
         utilities[name] = (description, func)
+        return func
     return wrapper
 
 
@@ -27,36 +29,64 @@ def util_bisect(*args):
         print(chatcommands.bisect.__func__(s) + "\n")
 
 
-@utility("exit", None)
+@utility("exit", "exit this utility (interactive mode)")
 def util_exit():
     exit()
 
 
-def main_start():
-    pass
+@utility("help", "display a list of available sub-commands")
+def util_help():
+    cmds = [(n, v[0], v[1]) for n, v in utilities.items()]
+    maxlen = max([len(x[0]) for x in cmds])
+    template = "  {{:{}}}  {{}}".format(maxlen)
+    print("List of available commands:\n" + "\n".join([template.format(n, d) for n, d, f in cmds]))
+
+
+def main_start_interactive():
+    try:
+        import readline
+    except ImportError:
+        pass
+    print("Smokey utility\nType \"help\" for a list of commands", file=sys.stderr)
 
 
 def main_loop(args):
-    name = args.pop(0)
+    try:
+        name = args.pop(0)
+    except IndexError:
+        exit()
     if name not in utilities:
         print("Error: function {!r} is not defined".format(name), file=sys.stderr)
+        return
 
     desc, func = utilities[name]
-    func(*args)
+    func_args = inspect.getargspec(func)
+    min_args = len(func_args[0] or "") - len(func_args[3] or "")
+    if len(args) < min_args:
+        print("Too few arguments")
+    elif not func_args[1] and len(args) > len(func_args[0] or ""):
+        print("Too many arguments")
+    else:
+        func(*args)
 
 
 def get_input():
-    s = input(">>> ")
-    while True:
-        try:
-            return shlex.split(s)
-        except ValueError:
-            s += input("... ")  # continue reading
+    try:
+        s = ""
+        s = input("$ ")
+        while True:
+            try:
+                return shlex.split(s)
+            except ValueError:
+                s += input("> ")  # continue reading
+    except EOFError:
+        print()
+        return [] if s else ["exit"]
 
 
 if __name__ == "__main__":
-    main_start()
     if len(sys.argv) == 1:
+        main_start_interactive()
         while True:
             main_loop(get_input())
     else:
