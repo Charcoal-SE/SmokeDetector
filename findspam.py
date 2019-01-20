258630f1ac928bb53ff89b709ac15a791a4403ee
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2019-01-20 15:02:00 +0800

    fix URL_REGEX DoS vulnerability, fix TAG_REGEX not matching </p>

--- findspam.py
+++ findspam.py
@@ -73,9 +73,9 @@ URL_REGEX = regex.compile(
     r"""(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2}))"""
     r"""(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])"""
     r"""(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))"""
-    r"""|(?:(?:[A-Za-z\u00a1-\uffff0-9]-?)*[A-Za-z\u00a1-\uffff0-9]+)(?:\.(?:[A-Za-z\u00a1-\uffff0-9]-?)"""
+    r"""|\b(?:(?:[A-Za-z\u00a1-\uffff0-9]-?)*[A-Za-z\u00a1-\uffff0-9]+)(?:\.(?:[A-Za-z\u00a1-\uffff0-9]-?)"""
     r"""*[A-Za-z\u00a1-\uffff0-9]+)*(?:\.(?:[A-Za-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/\S*)?""", regex.U)
-TAG_REGEX = regex.compile(r"</?[abcdehiklopsu][^>]+?>|\w+://", regex.U)
+TAG_REGEX = regex.compile(r"</?[abcdehiklopsu][^>]*?>|\w+://", regex.U)
 NUMBER_REGEX = regex.compile(r'(?<=\D|^)\+?(?:\d[\W_]*){8,13}\d(?=\D|$)', regex.U | regex.I)
 
 UNIFORM = math.log(1 / 36)
