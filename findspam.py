e689d625d66bbeebcaab5a0fad26040a66534a90
𝐢𝐁𝐮𝐠 ♦ <stevepad@me.com>
2019-02-12 21:52:48 +0800

    services? in <city>, autopull

--- findspam.py
+++ findspam.py
@@ -1682,7 +1682,7 @@ bad_keywords_nwb = [  # "nwb" == "no word boundary"
     " %[au]h ", "tapsi ?sarkar",
     "(?:networking|cisco|sas|hadoop|mapreduce|oracle|dba|php|sql|javascript|js|java|designing|marketing|"
     "salesforce|joomla)( certification)? (courses?|training)(?=.{,25}</a>)",
-    r"(?:design|development|compan(?:y|ies)|institute|training|courses?|jobs?|automation|sex|service)"
+    r"(?:design|development|compan(?:y|ies)|institute|training|courses?|jobs?|automation|sex|services?)"
     r"\W*(?:center|centre|institute|work|provider)?"
     r"(?:\b.{1,8}\b)?\L<city>\b",
     r"\b\L<city>(\b.{1,8}\b)?(?:tour)",  # TODO: Populate this "after city" keyword list
