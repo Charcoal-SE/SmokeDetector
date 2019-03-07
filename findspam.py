c4a85e10e446fb974c9ce292e3060a962ebf8f5e
BertieB <bertieb@bertieb.org>
2019-03-07 16:31:24 +0000

    Log (debug) ASN checks if skipped (+ lint fixes)
    Since there is a debug log statement in place for ASN checks already,
    skips should also be debug-logged. This produces little additional
    output in practice (<10 lines per 24h).

--- findspam.py
+++ findspam.py
@@ -60,10 +60,11 @@ WHITELISTED_WEBSITES_REGEX = regex.compile(r"(?i)upload|\b(?:{})\b".format("|".j
     r"jsfiddle\.net", r"codepen\.io", "pastebin"
 ] + [se_dom.replace(".", r"\.") for se_dom in SE_SITES_DOMAINS])))
 ASN_WHITELISTED_WEBSITES = ["docs.unity3d.com", "trac.ffmpeg.org", "bitcoincore.org", "ffmpeg.org",
-    "latex.codecogs.com", "www.advancedcustomfields.com", "name.com", "businessbloomer.com",
-    "wkhtmltopdf.org", "www.thefreedictionary.com", "ruby-doc.org", "www.site.com.br",
-    "www.ffmpeg.org", "unity3d.com", "test.ooo-pnu.ru", "swift.org", "site2.com", "rxweb.io",
-    "www.tenforums.com", "www.rhydolabz.com"]
+                            "latex.codecogs.com", "www.advancedcustomfields.com", "name.com",
+                            "businessbloomer.com", "wkhtmltopdf.org", "www.thefreedictionary.com",
+                            "ruby-doc.org", "www.site.com.br", "www.ffmpeg.org", "unity3d.com",
+                            "test.ooo-pnu.ru", "swift.org", "site2.com", "rxweb.io", "www.tenforums.com",
+                            "www.rhydolabz.com"]
 # ^^ top 20 FP hosts that get reported due to 'bad ASN', collated by regex parsing
 # https://metasmoke.erwaysoftware.com/data/sql/queries/164-bad-asn-in-false-positives
 COUNTRY = [
@@ -1164,7 +1165,10 @@ def bad_ip_for_url_hostname(s, site):
 
 def asn_for_url_host(s, site, asn_list):
     for hostname in post_hosts(s, check_tld=True):
-        if hostname in ASN_WHITELISTED_WEBSITES: continue
+        if hostname in ASN_WHITELISTED_WEBSITES:
+            log('debug', 'Skipping ASN check for hostname {1}'.format(
+                hostname))
+            continue
         a = dns_query(hostname, 'a')
         if a is not None:
             for addr in set([str(x) for x in a]):
