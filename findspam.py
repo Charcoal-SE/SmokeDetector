5b2331849d23856a85680aaed4240b73843cd53e
BertieB <bertieb@bertieb.org>
2019-03-08 14:30:31 +0000

    Whitespace '"." + x'

--- findspam.py
+++ findspam.py
@@ -1164,7 +1164,7 @@ def bad_ip_for_url_hostname(s, site):
 
 def asn_for_url_host(s, site, asn_list):
     for hostname in post_hosts(s, check_tld=True):
-        if any(hostname == x or hostname.endswith("."+x) for x in ASN_WHITELISTED_WEBSITES):
+        if any(hostname == x or hostname.endswith("." + x) for x in ASN_WHITELISTED_WEBSITES):
             log('debug', 'Skipping ASN check for hostname {0}'.format(
                 hostname))
             continue
