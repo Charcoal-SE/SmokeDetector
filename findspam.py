5704b4bbac170cdb57d832d811893a6ed918aaa2
tripleee <tripleee@users.noreply.github.com>
2019-04-11 14:37:13 +0300

    ! Watch Cloudflare NS pair elsa+theo

--- findspam.py
+++ findspam.py
@@ -1037,6 +1037,7 @@ def watched_ns_for_url_domain(s, site):
         {'duke.ns.cloudflare.com.', 'lola.ns.cloudflare.com.'},
         {'duke.ns.cloudflare.com.', 'wally.ns.cloudflare.com.'},
         {'elinore.ns.cloudflare.com.', 'sam.ns.cloudflare.com.'},
+        {'elsa.ns.cloudflare.com.', 'theo.ns.cloudflare.com.'},
         {'ernest.ns.cloudflare.com.', 'pat.ns.cloudflare.com.'},
         {'eva.ns.cloudflare.com.', 'hank.ns.cloudflare.com.'},
         {'glen.ns.cloudflare.com.', 'jean.ns.cloudflare.com.'},
