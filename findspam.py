54bad8ad28734dc47b48a005eeb2c4c7b415eafa
tripleee <tripleee@users.noreply.github.com>
2019-04-16 10:15:42 +0300

    ! Watch IP 162.254.252.93 (AS 55293 - A2HOSTING)

--- findspam.py
+++ findspam.py
@@ -1164,6 +1164,8 @@ def watched_ip_for_url_hostname(s, site):
             '166.62.6.66',      # fake-tech-support + hosting ms-officesetup etc
             '166.62.28.116',    # fake-tech-support printertechsupportnumbers.com
             '192.186.227.225',  # drugs topwellnessguru / healthcare350 etc
+            # AS 55293 A2HOSTING - A2 Hosting, Inc., US
+            '162.254.252.93',   # paper-writing etc appsocio doneessays etc
             # AS 394695 PUBLIC-DOMAIN-REGISTRY - PDR, US
             '116.206.104.141',  # fake-tech-support fake-diplomas
             '162.215.253.205',  # drugs escorts drozien.com nehasuri.in etc
