bdaa591c90e807ce4868d08a849aca7e500b9611
tripleee <tripleee@users.noreply.github.com>
2019-04-26 10:54:00 +0300

    ! Watch Cloudflare NS pair bruce+chan

--- findspam.py
+++ findspam.py
@@ -1057,6 +1057,7 @@ def watched_ns_for_url_domain(s, site):
         {'bonnie.ns.cloudflare.com.', 'guss.ns.cloudflare.com.'},
         {'brad.ns.cloudflare.com.', 'brenda.ns.cloudflare.com.'},
         {'brenda.ns.cloudflare.com.', 'theo.ns.cloudflare.com.'},
+        {'bruce.ns.cloudflare.com.', 'chan.ns.cloudflare.com.'},
         {'chip.ns.cloudflare.com.', 'cruz.ns.cloudflare.com.'},
         {'chris.ns.cloudflare.com.', 'tess.ns.cloudflare.com.'},
         {'dana.ns.cloudflare.com.', 'piotr.ns.cloudflare.com.'},
