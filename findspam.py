e23fe4639b162cdc48b1172e421442f4d65da6e1
tripleee <tripleee@users.noreply.github.com>
2019-06-06 13:51:05 +0300

    ! Watch ASN 133296 WEBWERKS-AS-IN Web Werks India

--- findspam.py
+++ findspam.py
@@ -1304,6 +1304,7 @@ def watched_asn_for_url_hostname(s, site):
             '55002',   # DEFENSE-NET - Defense.Net, Inc, US.
             '54290',   # HOSTWINDS - Hostwinds LLC., US
             '62731',   # 247RACK-COM - 247RACK.com, US
+            '133296',  # WEBWERKS-AS-IN Web Werks India Pvt. Ltd., IN
             '133711',  # HBSGZB-AS Home Broadband Services LLP, IN
             '197695',  # AS-REG, RU
             '200000',  # UKRAINE-AS, UA
