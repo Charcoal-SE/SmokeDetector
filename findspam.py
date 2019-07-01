32a82707d9008d108b35e8daf633fb55124f2eca
Makyen <wp.makyen@gmail.com>
2019-07-01 11:00:08 -0700

    Shorten comment to fix CI

--- findspam.py
+++ findspam.py
@@ -1933,12 +1933,12 @@ create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))keto(?:nes?)
             sites=['chemistry.stackexchange.com'],
             username=True, body_summary=True,
             max_rep=30, max_score=1)
-# Watch (?-i:SEO|seo)$, but exempt Webmasters for titles, but not usernames. Was a watch added by iBug on 1541730383. (part 1)
+# Watch (?-i:SEO|seo)$, but exempt Webmasters for titles, but not usernames. Was a watch by iBug on 1541730383. (pt1)
 create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))(?-i:SEO|seo)$",
             sites=['webmasters.stackexchange.com'],
             title=True, body=False, username=False,
             max_rep=30, max_score=1)
-# Watch (?-i:SEO|seo)$, but exempt Webmasters for titles, but not usernames. Was a watch added by iBug on 1541730383. (part 2)
+# Watch (?-i:SEO|seo)$, but exempt Webmasters for titles, but not usernames. Was a watch by iBug on 1541730383. (pt2)
 create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))(?-i:SEO|seo)$",
             title=False, body=False, username=True,
             max_rep=30, max_score=1)
