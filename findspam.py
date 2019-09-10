66471f9f722f07ff067aa64b6ebd75b46567cce3
tripleee <tripleee@users.noreply.github.com>
2019-09-10 13:50:27 +0300

    ! Watch IP 77.73.70.10

--- findspam.py
+++ findspam.py
@@ -1239,6 +1239,8 @@ def watched_ip_for_url_hostname(s, site):
             '166.62.6.66',      # fake-tech-support + hosting ms-officesetup etc
             '166.62.28.116',    # fake-tech-support printertechsupportnumbers.com
             '192.186.227.225',  # drugs topwellnessguru / healthcare350 etc
+            # AS-43317 FISHNET-AS, RU
+            '77.73.70.10',      # applesupportnumber.net
             # AS 46606 UNIFIEDLAYER-AS-1 - Unified Layer, US
             '50.87.189.77',     # avdshare.com
             '162.241.148.157',  # fake-tech-support en-mcafee setupmwnow etc
