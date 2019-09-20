db3506dc0cf579d00da5febba3209c4f7858fe34
Makyen <wp.makyen@gmail.com>
2019-09-19 18:06:58 -0700

    log exit_mode arguments

--- helpers.py
+++ helpers.py
@@ -28,6 +28,7 @@ def exit_mode(*args, code=0):
 
     with open("exit.txt", "w", encoding="utf-8") as f:
         print("\n".join(args), file=f)
+    log('debug', 'Exiting with args:' + ', '.join(args) or 'None')
     os._exit(code)
 
 
