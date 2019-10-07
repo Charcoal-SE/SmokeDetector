65d6ccd0bd626d5daa96ae1b466dc66a11c82627
iBug <git@ibugone.com>
2019-10-07 20:22:45 +0800

    Smokey can push without SSH now

--- docker-startup.sh
+++ docker-startup.sh
@@ -3,20 +3,11 @@
 # For use with Dockerfile
 
 check_ready() {
-  # Force "ready"
-  if [ -e ~/ready ]; then
+  # Expect the config file
+  if [ -r ~/SmokeDetector/config ]; then
     return 0
   fi
 
-  # Expect both SSH key and config file
-  if [ -r ~/.ssh/id_rsa -a -r ~/SmokeDetector/config ]; then
-    if [ $(stat -c %a ~/.ssh/id_rsa) != 600 ]; then
-      # SSH will surely complain about this
-      return 1
-    else
-      return 0
-    fi
-  fi
   return 1
 }
 
