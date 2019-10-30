ce83f319abed51e6d93ce4405ce0be25164603ef
ArtOfCode- <hello@artofcode.co.uk>
2019-10-30 21:34:42 +0000

    Flake

--- findspam.py
+++ findspam.py
@@ -1353,7 +1353,7 @@ def bad_ip_for_url_hostname(s, site):
 
 def asn_for_url_host(s, site, asn_list):
     for hostname in post_hosts(s, check_tld=True):
-        if any(hostname == x or hostname.endswith("." + x) or is_website_whitelisted(hostname)
+        if any(hostname == x or hostname.endswith("." + x) or metasmoke_cache.is_website_whitelisted(hostname)
                for x in ASN_WHITELISTED_WEBSITES):
             log('debug', 'Skipping ASN check for hostname {0}'.format(
                 hostname))
