85e5c608b0907c8617ed7f8ff9d23ec830923971
tripleee <tripleee@users.noreply.github.com>
2019-11-01 12:22:36 +0200

    ! Watch IP 162.241.216.230

--- findspam.py
+++ findspam.py
@@ -1302,6 +1302,7 @@ def watched_ip_for_url_hostname(s, site):
             '50.87.189.77',     # avdshare.com
             '162.241.148.13',   # fake-tech-support officekeyactivate en-us-mcafee etc
             '162.241.148.157',  # fake-tech-support en-mcafee setupmwnow etc
+            '162.241.216.230',  # aiobjectives.com
             '192.185.129.109',  # fake-tech-support 24x7technicalhelp etc
             '192.185.227.177',  # zooksoftware.com
             # AS 47583 (HOSTINGER) - also notice dual-homed below
