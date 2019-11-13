023f8a543aeff317f2b0f7500cddbdb273963b68
tripleee <tripleee@users.noreply.github.com>
2019-11-13 12:37:37 +0200

    ! Watch IP 204.11.59.175

--- findspam.py
+++ findspam.py
@@ -1354,6 +1354,7 @@ def watched_ip_for_url_hostname(s, site):
             '103.21.58.29',     # software-development infinigic / apporio.com
             '162.215.253.205',  # drugs escorts drozien.com nehasuri.in etc
             '204.11.58.189',    # visit-my-website random hits
+            '204.11.59.175',    # fake-tech-support brotherprinteuk etc
         ])
 
 
