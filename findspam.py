1ed0249609738cf318442f3c88c977d74d4382d8
tripleee <tripleee@users.noreply.github.com>
2019-11-14 08:02:51 +0200

    ! Watch IP 204.11.58.143

--- findspam.py
+++ findspam.py
@@ -1356,6 +1356,7 @@ def watched_ip_for_url_hostname(s, site):
             # AS 394695 PUBLIC-DOMAIN-REGISTRY - PDR, US
             '103.21.58.29',     # software-development infinigic / apporio.com
             '162.215.253.205',  # drugs escorts drozien.com nehasuri.in etc
+            '204.11.58.143',    # drugs wintersupplement ketopillsstore etc
             '204.11.58.189',    # visit-my-website random hits
             '204.11.59.175',    # fake-tech-support brotherprinteuk etc
         ])
