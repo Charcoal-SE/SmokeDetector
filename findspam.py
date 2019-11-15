991a2e0d1e548711a2c9ec665c2651803a2aecf9
tripleee <tripleee@users.noreply.github.com>
2019-11-15 17:23:55 +0200

    ! Watch IP 207.174.213.34
    Also add back commented-out Google IPs which have been listed in the past (related to this -- pivot was learntutorialspoint.com)

--- findspam.py
+++ findspam.py
@@ -1276,6 +1276,11 @@ def watched_ip_for_url_hostname(s, site):
             '23.236.62.147',    # random spam sites
             '35.202.42.57',     # fake-tech-support monktech etc
             '54.72.135.216',    # rootersports.com spectrum.chat etc
+            '207.174.213.34',   # random spam sites
+            #'216.239.32.21',    # random spam sites but many FPs
+            #'216.239.34.21',    # random spam sites but many FPs
+            #'216.239.36.21',    # random spam sites but many FPs
+            #'216.239.38.21',    # random spam sites but many FPs
             # AS 16276 OVH, FR
             '54.38.180.85',     # izmorelen.com
             # AS 16509 AMAZON-02 - Amazon.com, Inc., US
