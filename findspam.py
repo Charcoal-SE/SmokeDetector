bfe036ceb190781ac553ad143b9db5a08dd4e77d
tripleee <tripleee@users.noreply.github.com>
2019-11-15 17:11:52 +0200

    ! Watch IP 107.180.1.245

--- findspam.py
+++ findspam.py
@@ -1317,6 +1317,7 @@ def watched_ip_for_url_hostname(s, site):
             '23.229.180.169',   # drugs health4supplement / supplements4lifetime etc
             '23.229.233.231',   # drugs ultavivegarcinia.es / refollium.in etc
             '50.62.35.175',     # gs-jj patchesline.com shirtpatches.com
+            '107.180.1.245',    # mmf cash61.com fox120 (blogspam?)
             '107.180.1.245',    # mmf cash61.com pays12com
             '107.180.34.212',   # fake-tech-support 123helpline / allitexpert
             '107.180.39.226',   # drugs + escorts keto4diets etc + hotescortsjaipur
