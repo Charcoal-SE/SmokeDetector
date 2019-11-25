e885f4e5a9c7d5917b07f870d80cb90536311801
tripleee <tripleee@users.noreply.github.com>
2019-11-25 13:33:54 +0200

    ! Watch IP 199.188.200.90

--- findspam.py
+++ findspam.py
@@ -1331,6 +1331,7 @@ def watched_ip_for_url_hostname(s, site):
             '198.187.29.122',   # random hoshangabad.store darkhorsell.com
             '198.187.31.221',   # drugs portal4fitness move2.fitness etc
             '199.188.200.16',   # drugs totalketopills ketostorepills
+            '199.188.200.90',   # drugs goldencondor.org infervour.com
             '199.188.200.109',  # drugs atozfitnesstalks awaretalks etc
             '199.192.31.7',     # gambling indonesian IP-only
             # AS 23352 SERVERCENTRAL - Server Central Network, US
