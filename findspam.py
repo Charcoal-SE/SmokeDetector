432d13211003b7e14a3c1f8eb7a912f3513329e1
tripleee <tripleee@users.noreply.github.com>
2019-11-26 12:42:16 +0200

    ! Watch IP 162.251.85.134

--- findspam.py
+++ findspam.py
@@ -1434,6 +1434,7 @@ def watched_ip_for_url_hostname(s, site):
             '103.211.216.80',   # random powermindinstitute.in ooisolutions.com
             '162.215.241.244',  # drugs supplementstore4u + rvonlinegaming.com
             '162.215.253.205',  # drugs escorts drozien.com nehasuri.in etc
+            '162.251.85.134',   # fake-tech-support printers etc
             '204.11.58.143',    # drugs wintersupplement ketopillsstore etc
             '204.11.58.156',    # fake-tech-support smatustech comretail.net etc
             '204.11.58.189',    # visit-my-website random hits
