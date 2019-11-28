587387d43a7eb9deb102ec9008bcf38b9f009ab5
Double Beep <38133098+double-beep@users.noreply.github.com>
2019-11-28 16:19:30 +0200

    Remove trailing whitespace

--- findspam.py
+++ findspam.py
@@ -1326,7 +1326,7 @@ def watched_ip_for_url_hostname(s, site):
             '198.54.115.65',    # drugs goldencondor / visit4supplements etc
             '198.54.115.76',    # visit-my-website testmyinternetspeed.org etc
             '198.54.115.178',   # drugs + random dreewhispers.xyz
-            '198.54.116.24',    # drugs fitnesreviews pillsdrive 
+            '198.54.116.24',    # drugs fitnesreviews pillsdrive
             '198.54.116.51',    # drugs reviewsgear / crazy-bulk-review etc
             '198.54.116.71',    # drugs + fake-tech-support guidemenutrition etc
             '198.54.116.85',    # drugs mummydiet.org
