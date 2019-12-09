2c54a80bb7f03bed5a0fc904b271bc0157abfc86
iBug <git@ibugone.com>
2019-12-09 13:42:11 +0800

    Docker run script: Ensure GitHub exists in SSH known_hosts

--- docker-startup.sh
+++ docker-startup.sh
@@ -13,6 +13,13 @@ check_ready() {
 
 cd ~smokey/SmokeDetector
 
+# Fetch GitHub host key if using SSH
+if [ -r "$HOME/.ssh/id_rsa" ] || ! git config --get core.sshCommand; then
+  if ! ssh-keygen -F github.com >/dev/null 2>&1; then
+    ssh-keyscan 2>/dev/null github.com >> ~/.ssh/known_hosts
+  fi
+fi
+
 while ! check_ready; do
   sleep 1
 done
