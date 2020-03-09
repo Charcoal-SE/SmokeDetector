b2fb78e8d9cd835406d89b5b1b2068663cb40683
Makyen <wp.makyen@gmail.com>
2020-03-09 11:33:04 -0700

    Watch poo+p?(?:y|ie)?s? on The Workplace, due to a persistent troll.
    -autopull

--- findspam.py
+++ findspam.py
@@ -1758,6 +1758,11 @@ create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))(?-i:SEO|seo
 create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))(?-i:SEO|seo)$",
             title=False, body=False, username=True,
             max_rep=30, max_score=1)
+# Watch poo+p?(?:y|ie)?s? on The Workplace, due to a persistent spammer
+create_rule("potentially bad keyword in {}", r"(?is)(?:^|\b|(?w:\b))poo+p?(?:y|ie)?s?(?:\b|(?w:\b)|$)", all=False,
+            sites=['workplace.stackexchange.com'],
+            username=True, body_summary=True,
+            max_rep=30, max_score=1)
 # Bad keywords in titles and usernames, all sites
 create_rule("bad keyword in {}",
             r"(?i)^(?:(?=.*?\b(?:online|hd)\b)(?=.*?(?:free|full|unlimited)).*?movies?\b)|(?=.*?\b(?:acai|"
