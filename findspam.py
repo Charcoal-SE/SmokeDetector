95ca4ceb8dc594a4b7741d3618421ff656f35ba4
tripleee <tripleee@users.noreply.github.com>
2020-03-20 06:54:55 +0200

    Add nirsoft.net to ASN whitelist
    This has been a recurring FP (0/10 so far) and is now also covered by the watch on their ASN

--- findspam.py
+++ findspam.py
@@ -94,6 +94,7 @@ ASN_WHITELISTED_WEBSITES = [
     "wikileaks.org",
     # Additional added not as part of a systematic investigation:
     "ntp.org", "cpu-world.com", "caniuse.com", "guru99.com", "fontawesome.com",
+    "nirsoft.net",
     # Added to prevent having 3 detections on just the domain.
     "writingexplained.org", "eitren.com"]
 
