8d3df84b4ce248db5044eef967fefd4fd57f5b01
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-05-16 03:10:18 -0400

    Update metasmoke.py

--- metasmoke.py
+++ metasmoke.py
@@ -273,7 +273,8 @@ class Metasmoke:
         threshold = 1
         GlobalVars.posts_scan_stats_lock.acquire()
         if (GlobalVars.num_posts_scanned < threshold):
-            chatcommunicate.tell_rooms_with("debug","Only {} posts scanned in the last minute. Something might be wrong.".format(GlobalVars.num_posts_scanned))
+            chatcommunicate.tell_rooms_with("debug", \
+            "Only {} posts scanned in the last minute. Something might be wrong.".format(GlobalVars.num_posts_scanned))
         GlobalVars.posts_scan_stats_lock.release()
 
         if GlobalVars.metasmoke_host is None:
