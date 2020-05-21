8950c8fc7c5abfff0ed8ceb9c3f283da7db29a4e
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-05-20 22:23:55 -0400

    Adjust testing cases to fit current code logic Adjusted testing cases for chatcommands regarding metasmoke to suit currently implemented code logic.

--- test/test_chatcommands.py
+++ test/test_chatcommands.py
@@ -24,6 +24,10 @@ from fake import Fake
 from unittest.mock import patch
 
 
+def dummy_tell_rooms_with(dummy1, dummy2):
+    pass
+
+
 def test_null():
     assert chatcommands.null() is None
 
@@ -562,6 +566,8 @@ def test_whitelisted_users():
 
 
 def test_metasmoke():
+    orig_tell_rooms_with = chatcommunicate.tell_rooms_with
+    chatcommunicate.tell_rooms_with = dummy_tell_rooms_with
     msg = Fake({
         "owner": {
             "name": "ArtOfCode",
@@ -580,12 +586,13 @@ def test_metasmoke():
     })
     msg_source = "metasmoke is {}. Current failure count: {} " + "({id})".format(id=GlobalVars.location)
 
-    assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-up") == "metasmoke is now considered up."
+    chatcommands.metasmoke(original_msg=msg, alias_used="ms-up")
+    assert GlobalVars.MSStatus.is_up()
+    chatcommands.metasmoke(original_msg=msg, alias_used="ms-down")
+    assert GlobalVars.MSStatus.is_down()
+    GlobalVars.MSStatus.reset_ms_status()
     assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-status") == msg_source.format("up", 0)
-    assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-down") == "metasmoke is now considered down."
-    assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-status") == msg_source.format("down", 999)
-    assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-up") == "metasmoke is now considered up."
-    assert chatcommands.metasmoke(original_msg=msg, alias_used="ms-status") == msg_source.format("up", 0)
+    chatcommunicate.tell_rooms_with = orig_tell_rooms_with
 
 
 @pytest.mark.skipif(os.path.isfile("notifications.p"), reason="shouldn't overwrite file")
