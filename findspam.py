02b111db143e0fc7eec399edada19d354affb590
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-06-21 15:48:24 -0400

    Use consistent quantifiers

--- findspam.py
+++ findspam.py
@@ -1698,9 +1698,9 @@ pattern_websites = [
     # The following may have been intended to include (?:yahoo|gmail|hotmail|outlook|office|microsoft)?[\w-]{0,10}
     # But, the regex made that superfluous.
     r"http\S*?"
-    r"(?:accounts?+|tech|customers?+|supports?+|services?+|phones?|helps?+)"
+    r"(?:accounts?+|tech|customers?+|supports?+|services?+|phones?+|helps?+)"
     r"[\w-]{0,10}"
-    r"(?:services?+|cares?+|helps?+|recover(?:y|ies)|supports?|phones?|numbers?+)",
+    r"(?:services?+|cares?+|helps?+|recover(?:y|ies)|supports?+|phones?+|numbers?+)",
 
     r"http\S*?(?:essay|resume|thesis|dissertation|paper)-?writing",
     r"fix[\w-]*?(?:files?|tool(?:box)?)\.com",
