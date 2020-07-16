4f975748529c827e0d43641340dbf90e343d4c57
Daniil-M-beep <64322880+Daniil-M-beep@users.noreply.github.com>
2020-07-16 15:49:30 +0100

    Update gitmanager.py

--- gitmanager.py
+++ gitmanager.py
@@ -349,9 +349,13 @@ class GitManager:
         if not response:
             raise ConnectionError("Cannot connect to GitHub API")
         pr_info = response.json()
-
+        if pr_info["user"]["login"] != "SmokeDetector":
+            raise ValueError("PR #{} is not created by me, so I can't approve it.".format(pr_id))
         if "<!-- METASMOKE-BLACKLIST" not in pr_info["body"]:
             raise ValueError("PR description is malformed. Blame a developer.")
+        if pr_info["state"] != "open":
+            raise ValueError("PR #{} is not currently open, so I won't merge it.".format(pr_id))
+        ref = pr_info['head']['ref']
         string = pr_into["title"]
         string = regex.match(r".*?: \S+ (.*?)(?:\Z|\s*\(\?#)", str).group(1)
         string = string.replace("\\", "")
