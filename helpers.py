eb70b7b3e21016100305874b0231456b011501a7
user12986714 <65436504+user12986714@users.noreply.github.com>
2020-10-07 17:40:44 -0400

    Use sys.exit

--- helpers.py
+++ helpers.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-import os
 import sys
 import traceback
 from datetime import datetime
@@ -29,7 +28,7 @@ def exit_mode(*args, code=0):
     with open("exit.txt", "w", encoding="utf-8") as f:
         print("\n".join(args), file=f)
     log('debug', 'Exiting with args: {}'.format(', '.join(args) or 'None'))
-    os._exit(code)
+    sys.exit(code)
 
 
 class ErrorLogs:
