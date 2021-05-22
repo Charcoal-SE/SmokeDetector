fb329b84c67d0fcf51503307ff201743997a7c92
Makyen <wp.makyen@gmail.com>
2021-05-22 04:55:51 -0700

    Revert "Don't verify metasmoke SSL certificate -autopull"
    This reverts commit 3250c4b881a06c59aefc90997c404c081e1850a0.

--- metasmoke.py
+++ metasmoke.py
@@ -30,7 +30,6 @@ from gitmanager import GitManager
 import findspam
 from socketscience import SocketScience
 import metasmoke_cache
-from ssl import CERT_NONE
 
 
 MS_WEBSOCKET_LONG_INTERVAL = 60
@@ -153,8 +152,7 @@ class Metasmoke:
     @staticmethod
     def connect_websocket():
         GlobalVars.metasmoke_ws = websocket.create_connection(GlobalVars.metasmoke_ws_host,
-                                                              origin=GlobalVars.metasmoke_host,
-                                                              sslopt={"cert_reqs": CERT_NONE})
+                                                              origin=GlobalVars.metasmoke_host)
         payload = json.dumps({"command": "subscribe",
                               "identifier": "{\"channel\":\"SmokeDetectorChannel\","
                               "\"key\":\"" + GlobalVars.metasmoke_key + "\"}"})
@@ -635,7 +633,6 @@ class Metasmoke:
 
             response = None  # Should return None upon failure, if any
             try:
-                kwargs['verify'] = False  # Don't verify metasmokes's SSL cert.
                 response = method(GlobalVars.metasmoke_host + url, *args, **kwargs)
             except Exception:
                 GlobalVars.MSStatus.failed()
