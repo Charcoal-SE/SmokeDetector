247bac749a0ae87f6e877fb5859fe958c5798966
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2021-09-29 13:52:53 -0700

    we really can't use sys.exit after all
    Add a comment, because this keeps tripping people up:
      https://chat.stackexchange.com/transcript/11540?m=48239423

--- ws.py
+++ ws.py
@@ -14,7 +14,6 @@ install_thread_excepthook()
 # test it thoroughly.
 
 import os
-import atexit
 import platform
 # noinspection PyPackageRequirements
 import websocket
@@ -93,9 +92,6 @@ if not GlobalVars.metasmoke_key:
     log('info', "No metasmoke key found, which is okay if both are running on the same host")
 if not GlobalVars.metasmoke_ws_host:
     log('info', "No metasmoke websocket host found, which is okay if you're anti-websocket")
-
-# Register actually_add_queue_timings_data hook
-atexit.register(actually_add_queue_timings_data)
 
 # Initiate DNS
 #
