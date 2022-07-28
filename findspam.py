7e7946c314cb37f318397a2f08056a6ad4ee54a1
Makyen <makyen@users.noreply.github.com>
2022-07-28 14:35:45 -0700

    post is mostly images: ignore images in code

--- findspam.py
+++ findspam.py
@@ -24,7 +24,7 @@ import dns.resolver
 import requests
 import chatcommunicate
 
-from helpers import log, regex_compile_no_cache, strip_pre_and_code_elements
+from helpers import log, regex_compile_no_cache, strip_pre_and_code_elements, strip_code_elements
 import metasmoke_cache
 from globalvars import GlobalVars
 import blacklists
@@ -888,7 +888,7 @@ def mostly_img(s, site):
     if len(s) == 0:
         return False, ""
 
-    s_len_img = len_img_block(s)
+    s_len_img = len_img_block(strip_code_elements(s))
     if s_len_img / len(s) > IMG_TXT_R_THRES:
         return True, "{:.4f} of the post is html image blocks".format(s_len_img / len(s))
     return False, ""
