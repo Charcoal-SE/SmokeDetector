c2d009363ff53c99fb5cbe5517c4e059cd1e0e46
tripleee <tripleee@users.noreply.github.com>
2022-11-29 08:34:13 +0200

    findspam.py: Add "consultant" to thing in L<city>

--- findspam.py
+++ findspam.py
@@ -2047,7 +2047,7 @@ bad_keywords_nwb = [  # "nwb" == "no word boundary"
 
     r"(?:"
     "" r"design|development|compan(?:y|ies)|agen(?:ts?|c(?:y|ies))|experts?|institutes?|classes|schools?"
-    "" r"|colleges?|universit(?:y|ies)|training|courses?|jobs?|institutions?"
+    "" r"|colleges?|universit(?:y|ies)|training|courses?|jobs?|institutions?|consultants?"
     "" r"|automation|sex|services?|kindergarten"
     "" r"|services?|maintenance|clinic|surgeons?|treatments?|rehabilitation"
     "" r"|studios?|designers?"
