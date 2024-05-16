53eca39b10336b7b8a93e9ad666b06822a4aa2c6
tripleee <tripleee@users.noreply.github.com>
2024-05-16 15:51:03 +0300

    Disable redirector test for t.ly/AqjA
    This test started failing because of a CAPTCHA problem
    
    See https://github.com/Charcoal-SE/SmokeDetector/issues/11151

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -6,7 +6,9 @@ import helpers
 
 @pytest.mark.skipif("CHINA" in os.environ, reason="")
 @pytest.mark.parametrize('shortened, original', [
-    ('https://t.ly/AqjA', 'https://charcoal-se.org/smokey/'),
+    # FIXME: disabled for now
+    # See bug report https://github.com/Charcoal-SE/SmokeDetector/issues/11151
+    # ('https://t.ly/AqjA', 'https://charcoal-se.org/smokey/'),
     ('https://bit.ly/2jhMbxn', 'https://charcoal-se.org/'),
 ])
 def test_unshorten_link(shortened, original):
