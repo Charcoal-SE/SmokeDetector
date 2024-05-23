2e7c94502350b519d80cb2235b7ddbc75102e681
Christopher White <cjwhiteusa@yahoo.com>
2024-05-23 16:43:34 -0400

    Update chatcommands.py

--- chatcommands.py
+++ chatcommands.py
@@ -721,6 +721,7 @@ def reject(msg, args, alias_used="reject"):
     pr_body = pr_object.json()['body']
     pr_author_id = regex.search(r"(?<=\/users\/)\d+", pr_body).group(0)
     print(pr_author_id)
+    print(msg.owner.id)
     self_reject = pr_author_id == msg.owner.id
     if not code_permissions and not self_reject:
         raise CmdException("You need blacklist manager privileges to reject pull requests " \
