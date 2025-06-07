443437f0f88f2f46fef840364459bb9f530bbb81
Daniel Getz <dan@getzit.net>
2025-06-07 02:02:38 -0400

    util.py: run commands on single arguments - if it can be single line, run it as single line - detect multi-line input with starting quotes, and handle similar to before - no longer exit on a blank interactive input line

--- util.py
+++ util.py
@@ -6,7 +6,6 @@ import shutil
 import sys
 import os
 import shlex
-import inspect
 from tempfile import mkstemp
 
 import helpers
@@ -37,63 +36,53 @@ def utility(name_s, description):
 
 
 @utility("bisect", "finds out which blacklist/watchlist item matches a string")
-def util_bisect(*args):
-    for s in args:
-        print(chatcommands.bisect.__func__(None, s) + "\n")
+def util_bisect(arg):
+    print(chatcommands.bisect.__func__(None, arg) + "\n")
 
 
 @utility("bisect-number", "finds out which number blacklist/watchlist item matches a number")
-def util_bisect_number(*args):
-    for s in args:
-        print(chatcommands.bisect_number.__func__(None, s) + "\n")
+def util_bisect_number(arg):
+    print(chatcommands.bisect_number.__func__(None, arg) + "\n")
 
 
 @utility("deobfuscate-number", "homoglyph deobfuscate and normalize a number pattern")
-def util_deobfuscate_number(*args):
-    for s in args:
-        print(chatcommands.deobfuscate_number.__func__(None, s) + "\n")
+def util_deobfuscate_number(arg):
+    print(chatcommands.deobfuscate_number.__func__(None, arg) + "\n")
 
 
 @utility("normalize-number", "show the number normalizations for a number pattern")
-def util_normalize_number(*args):
-    for s in args:
-        print(chatcommands.normalize_number.__func__(None, s) + "\n")
+def util_normalize_number(arg):
+    print(chatcommands.normalize_number.__func__(None, arg) + "\n")
 
 
 @utility(["test-question", "test-q"], "test if text would be automatically reported as a question body")
-def util_test_question(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test-question') + "\n")
+def util_test_question(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test-question') + "\n")
 
 
 @utility(["test-answer", "test-a"], "test if text would be automatically reported as an answer body")
-def util_test_answer(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test-answer') + "\n")
+def util_test_answer(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test-answer') + "\n")
 
 
 @utility(["test-user", "test-u"], "test if text would be automatically reported as username")
-def util_test_user(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test-user') + "\n")
+def util_test_user(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test-user') + "\n")
 
 
 @utility(["test-title", "test-t"], "test if text would be automatically reported as a question title")
-def util_test_title(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test-title') + "\n")
+def util_test_title(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test-title') + "\n")
 
 
 @utility("test", "test if text would be automatically reported as a post, title, or username")
-def util_test(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test') + "\n")
+def util_test(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test') + "\n")
 
 
 @utility(["test-json", "test-j"], "test if post, given in JSON, would be automatically reported")
-def util_test_json(*args):
-    for s in args:
-        print(chatcommands.test.__func__(s, alias_used='test-json') + "\n")
+def util_test_json(arg):
+    print(chatcommands.test.__func__(arg, alias_used='test-json') + "\n")
 
 
 # fake blacklist by copying it to a temporary file, so we can modify it
@@ -115,38 +104,34 @@ def ensure_blacklist_faked(blacklist_name: str):
 
 
 @utility("blacklist-keyword", "pretend a keyword has been added to the blacklist")
-def util_blacklist_keyword(*args):
+def util_blacklist_keyword(arg):
     ensure_blacklist_faked('KEYWORDS')
     blacklist = Blacklist(Blacklist.KEYWORDS)
-    for s in args:
-        blacklist.add(s)
+    blacklist.add(arg)
     findspam.FindSpam.reload_blacklists()
 
 
 @utility("blacklist-website", "pretend a website has been added to the blacklist")
-def util_blacklist_website(*args):
+def util_blacklist_website(arg):
     ensure_blacklist_faked('WEBSITES')
     blacklist = Blacklist(Blacklist.WEBSITES)
-    for s in args:
-        blacklist.add(s)
+    blacklist.add(arg)
     findspam.FindSpam.reload_blacklists()
 
 
 @utility("blacklist-username", "pretend a username has been added to the blacklist")
-def util_blacklist_username(*args):
+def util_blacklist_username(arg):
     ensure_blacklist_faked('USERNAMES')
     blacklist = Blacklist(Blacklist.USERNAMES)
-    for s in args:
-        blacklist.add(s)
+    blacklist.add(arg)
     findspam.FindSpam.reload_blacklists()
 
 
 @utility("blacklist-number", "pretend a number has been added to the blacklist")
-def util_blacklist_number(*args):
+def util_blacklist_number(arg):
     ensure_blacklist_faked('NUMBERS')
     blacklist = Blacklist(Blacklist.NUMBERS)
-    for s in args:
-        blacklist.add(s)
+    blacklist.add(arg)
     findspam.FindSpam.reload_blacklists()
 
 
@@ -161,21 +146,17 @@ def unblacklist(pattern):
 
 
 @utility("unblacklist", "pretend it has been removed from the blacklists")
-def util_unblacklist(*args):
-    changes_made = False
-    for s in args:
-        removed_from = unblacklist(s)
-        if removed_from is not None:
-            print("Removed `{}` from {}.\n".format(s, removed_from))
-            changes_made = True
-        else:
-            print("No such item `{}` in blacklist.\n".format(s))
-    if changes_made:
+def util_unblacklist(s):
+    removed_from = unblacklist(s)
+    if removed_from is not None:
+        print("Removed `{}` from {}.\n".format(s, removed_from))
         findspam.FindSpam.reload_blacklists()
+    else:
+        print("No such item `{}` in blacklist.\n".format(s))
 
 
 @utility("exit", "exit this utility (interactive mode)")
-def util_exit():
+def util_exit(_arg=''):
     # delete all temporary blacklist copies
     for path in tmp_blacklist_paths.values():
         os.unlink(path)
@@ -183,59 +164,60 @@ def util_exit():
 
 
 @utility("help", "display a list of available sub-commands")
-def util_help():
+def util_help(_arg=''):
     cmds = [(n, v[0], v[1]) for n, v in utilities.items()]
     maxlen = max([len(x[0]) for x in cmds])
     template = "  {{:{}}}  {{}}".format(maxlen)
     print("List of available commands:\n" + "\n".join([template.format(n, d) for n, d, f in cmds]))
 
 
-def main_start_interactive():
+def interactive_loop():
     try:
         import readline
     except ImportError:
         pass
     print("Smokey utility\nType \"help\" for a list of commands", file=sys.stderr)
+    while True:
+        cmd = input_command()
+        if cmd:
+            run_command(*cmd)
 
 
-def main_loop(args):
-    try:
-        name = args.pop(0)
-    except IndexError:
-        exit()
+def run_command(name, arg):
     if name not in utilities:
         print("Error: function {!r} is not defined".format(name), file=sys.stderr)
         return
 
     desc, func = utilities[name]
-    func_args = inspect.getfullargspec(func)
-    min_args = len(func_args[0] or "") - len(func_args[3] or "")
-    if len(args) < min_args:
-        print("Too few arguments")
-    elif not func_args[1] and len(args) > len(func_args[0] or ""):
-        print("Too many arguments")
-    else:
-        func(*args)
+    func(arg)
 
 
-def get_input():
+def input_command():
+    s = ""
     try:
-        s = ""
         s = input("$ ")
-        while True:
-            try:
-                return shlex.split(s)
-            except ValueError:
-                s += input("> ")  # continue reading
+        s_split = s.split(maxsplit=1)
+        if not s_split:
+            return None
+        cmd, arg = s_split[0], s_split[1] if len(s_split) > 1 else ''
+        if arg and arg[0] in ('"', "'"):
+            arg = input_multiline_arg(arg)
+        return cmd, arg
     except EOFError:
         print()
-        return [] if s else ["exit"]
+        return None if s else ("exit", "")
+
+
+def input_multiline_arg(s):
+    while True:
+        try:
+            return shlex.join(shlex.split(s))
+        except ValueError:
+            s += "\n" + input("> ")  # continue reading
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        main_start_interactive()
-        while True:
-            main_loop(get_input())
+        interactive_loop()
     else:
-        main_loop(sys.argv[1:])
+        run_command(sys.argv[1], shlex.join(sys.argv[1:]))
