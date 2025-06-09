26d2a0a73e9d9aa44b938006e59627866e3cf509
Daniel Getz <dan@getzit.net>
2025-06-09 16:41:37 -0400

    util.py: add command line arguments - still allows command line arguments, but now allows them to be treated individually   or as a single line with -S - force interactive mode with -i after running other commands - specfify command more explicitly with -c - load commands from a file/pipe with -C - load arguments to the specified command from a file/pipe with -A

--- util.py
+++ util.py
@@ -165,10 +165,14 @@ def util_exit(_arg=''):
 
 @utility("help", "display a list of available sub-commands")
 def util_help(_arg=''):
+    print(command_help_text())
+
+
+def command_help_text() -> str:
     cmds = [(n, v[0], v[1]) for n, v in utilities.items()]
     maxlen = max([len(x[0]) for x in cmds])
     template = "  {{:{}}}  {{}}".format(maxlen)
-    print("List of available commands:\n" + "\n".join([template.format(n, d) for n, d, f in cmds]))
+    return "List of available commands:\n" + "\n".join([template.format(n, d) for n, d, f in cmds])
 
 
 def interactive_loop():
@@ -177,47 +181,147 @@ def interactive_loop():
     except ImportError:
         pass
     print("Smokey utility\nType \"help\" for a list of commands", file=sys.stderr)
-    while True:
-        cmd = input_command()
-        if cmd:
-            run_command(*cmd)
+    run_full_commands()
+
+
+def run_full_commands(file=None, cmd=None):
+    """Runs a number of commands from the given file, or interactively
+
+    If file is None, runs interactively.
+    If cmd is specified, the command's arguments only will come from file.
+    """
+    try:
+        while True:
+            full_cmd = get_full_command(cmd=cmd, file=file)
+            if full_cmd:
+                run_command(*full_cmd)
+    except EOFError:
+        pass
 
 
 def run_command(name, arg):
     if name not in utilities:
         print("Error: function {!r} is not defined".format(name), file=sys.stderr)
         return
-
     desc, func = utilities[name]
     func(arg)
 
 
-def input_command():
-    s = ""
-    try:
-        s = input("$ ")
-        s_split = s.split(maxsplit=1)
-        if not s_split:
+def get_full_command(file=None, cmd=None):
+    """Gets a command from the given file, or interactively
+
+    If file is None, gets the command and argument from interactive input.
+    If cmd is specified, the command's argument only will come from file.
+
+    Returns None if the input was blank.
+    """
+    arg = get_input(file)
+    if cmd is None:
+        arg_split = arg.split(maxsplit=1)
+        if not arg_split:
             return None
-        cmd, arg = s_split[0], s_split[1] if len(s_split) > 1 else ''
-        if arg and arg[0] in ('"', "'"):
-            arg = input_multiline_arg(arg)
-        return cmd, arg
-    except EOFError:
-        print()
-        return None if s else ("exit", "")
+        cmd, arg = arg_split[0], arg_split[1] if len(arg_split) > 1 else ''
+    if arg and arg[0] in ('"', "'"):
+        arg = get_multiline_arg(file, arg)
+    return cmd, arg
 
 
-def input_multiline_arg(s):
-    while True:
-        try:
-            return shlex.join(shlex.split(s))
-        except ValueError:
-            s += "\n" + input("> ")  # continue reading
+def get_multiline_arg(file, arg):
+    """Continues receiving a multiline argument from the given file, or interactively
+
+    If file is None, gets lines of text from interactive input.
+    """
+    value_error = None
+    try:
+        while True:
+            try:
+                return shlex.join(shlex.split(arg))
+            except ValueError as e:
+                # Assume we have more input, so don't raise an error yet.
+                value_error = e
+                arg += "\n" + get_input(file, is_continuation=True)  # continue reading
+    except EOFError as e:
+        # Raise the ValueError if the argument was not terminated.
+        raise value_error if value_error else e
+
+
+def get_input(file=None, is_continuation=False):
+    """Gets a line of input from the given file, or interactively
+
+    If file is None, uses interactive input.
+    If is_continuation is True, this is the continuation of a multi-line argument.
+
+    Raises EOFError when there is no more input.
+    """
+    if file is None:
+        return input("> " if is_continuation else "$ ")
+    else:
+        line = file.readline()
+        if line:
+            return line.rstrip('\n')
+        else:
+            raise EOFError
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    import argparse
+
+    def file_arg(file_type):
+        """For storing all specified files in order, but remembering their type"""
+        return lambda filename: (argparse.FileType('r')(filename), file_type)
+
+    argv_parser = argparse.ArgumentParser(epilog=command_help_text(),
+                                          formatter_class=argparse.RawTextHelpFormatter)
+    argv_parser.add_argument("-c", "--cmd",
+                             help="Name of command to run")
+    argv_parser.add_argument("-i", "--interactive", action='store_true', default=None,
+                             help="Interactive mode (the default if no command or files are specified)")
+    argv_parser.add_argument("-S", "--single", action='store_true',
+                             help="Join further command line arguments into a single command argument"
+                                  + "\nOtherwise, they will be treated as individual arguments")
+    argv_parser.add_argument("-f", "--file",
+                             type=file_arg('f'), action='append',
+                             help="Load commands or arguments from a file (guesses which you mean)")
+    argv_parser.add_argument("-C", "--cmd-file", metavar='FILE',
+                             type=file_arg('C'), action='append', dest='file',
+                             help="Load commands from a file")
+    argv_parser.add_argument("-A", "--arg-file", metavar='FILE',
+                             type=file_arg('A'), action='append', dest='file',
+                             help="Load arguments from a file")
+    argv_parser.add_argument("args", nargs='*',
+                             help="The command followed by its arguments"
+                                  + "\nOr just the arguments if the command is specified with -c")
+
+    options = argv_parser.parse_args()
+    # load cmd from command line if not specified with an option
+    if not options.cmd and options.args:
+        options.cmd, options.args = options.args[0], options.args[1:]
+
+    # default to interactive mode if it makes sense
+    if options.interactive is None and options.file:
+        for file, file_type in options.file:
+            if file.name == '<stdin>':
+                options.interactive = False
+    if options.interactive is None and not options.cmd:
+        options.interactive = True
+
+    if options.single and options.args:
+        options.args = [shlex.join(options.args)]
+
+    if options.file and not options.cmd and any(file_type == 'A' for file, file_type in options.file):
+        argv_parser.error("--arg-file specified, but no command specified")
+
+    # Run command line commands
+    if options.cmd:
+        for arg in options.args:
+            run_command(options.cmd, arg)
+
+    # Run commands from files
+    if options.file:
+        for file, file_type in options.file:
+            if file_type == 'f':
+                file_type = 'A' if options.cmd else 'C'
+            run_full_commands(file=file, cmd=options.cmd if file_type == 'A' else None)
+
+    if options.interactive:
         interactive_loop()
-    else:
-        run_command(sys.argv[1], shlex.join(sys.argv[1:]))
