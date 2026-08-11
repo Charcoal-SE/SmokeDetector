12a7f822e15584f9f5218f67501f6f92ee4dfe61
iBug <git@ibugone.com>
2026-08-11 10:20:09 +0800

    test: suppress warnings with pytest.toml

--- test/conftest.py
+++ test/conftest.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock
 import os
 import sys
-import warnings
 
 
 def raise_system_exit(code):
@@ -10,5 +9,3 @@ def raise_system_exit(code):
 
 os._exit = MagicMock(spec=os._exit, wraps=None, side_effect=raise_system_exit)  # Prevent exit from os._exit()
 sys.exit = MagicMock(spec=sys.exit, wraps=None, side_effect=raise_system_exit)  # Prevent exit from sys.exit()
-
-warnings.filterwarnings(action="ignore", message=r"datetime.datetime.utcnow")
