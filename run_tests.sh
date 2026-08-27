9713233bc349168304cf8eb37a25ca5dabe9060d
iBug <git@ibugone.com>
2026-08-27 03:37:28 +0000

    run_tests.sh: use ruff instead of flake8
    Co-authored-by: iBug <7273074+iBug@users.noreply.github.com>

--- run_tests.sh
+++ run_tests.sh
@@ -3,14 +3,8 @@
 # Abusing the shell no-op (colon) for outputting
 set -x
 
-: Lint tests
-python3 -m flake8 --config=tox_tests.ini ./test/
-
-: Lint classes
-python3 -m flake8 --config=tox_classes.ini ./classes/
-
 : Lint code
-python3 -m flake8 ./*.py
+ruff check
 
 : Pytest
 python3 -W default::Warning -m pytest test
